967ab0175e35bdf7cc1c566739a47d0d7d0ff20a
Iñaki Baz Castillo <ibc@aliax.net>
2016-02-20 18:51:27 +0100

    More test units

--- test/test_scene_1.js
+++ test/test_scene_1.js
@@ -13,8 +13,15 @@ tap.test('alice and bob create RtpReceivers and expect RtpSenders', { timeout: 2
 
 	let room = server.Room();
 
+	t.pass('room created');
+
 	let alice = room.Peer('alice');
+
+	t.pass('alice created');
+
 	let bob = room.Peer('bob');
+
+	t.pass('bob created');
 
 	let aliceAudioParameters =
 	{
@@ -146,17 +153,133 @@ tap.test('alice and bob create RtpReceivers and expect RtpSenders', { timeout: 2
 		])
 		.then(() =>
 		{
-			t.equal(alice.getRtcSenders().length, 1, '`alice.getRtcSenders() must retrieve 1');
-			t.equal(bob.getRtcSenders().length, 2, '`bob.getRtcSenders() must retrieve 2');
+			t.equal(alice.getRtpSenders().length, 1, 'alice.getRtpSenders() must retrieve 1');
+			t.equal(bob.getRtpSenders().length, 2, 'bob.getRtpSenders() must retrieve 2');
 
-			t.same(alice.getRtcSenders()[0].rtpParameters, bobVideoParameters,
+			t.same(alice.getRtpSenders()[0].rtpParameters, bobVideoParameters,
 				'first RtpSender parameters of alice must match video parameters of bob');
-			t.same(bob.getRtcSenders()[0].rtpParameters, aliceAudioParameters,
+			t.same(bob.getRtpSenders()[0].rtpParameters, aliceAudioParameters,
 				'first RtpSender parameters of bob must match audio parameters of alice');
-			t.same(bob.getRtcSenders()[1].rtpParameters, aliceVideoParameters,
+			t.same(bob.getRtpSenders()[1].rtpParameters, aliceVideoParameters,
 				'second RtpSender parameters of bob must match video parameters of alice');
+		})
+		.then(() =>
+		{
+			let aliceAudioReceiver = alice.getRtpReceivers()[0];
+			let aliceVideoReceiver = alice.getRtpReceivers()[1];
+			let aliceVideoSender = alice.getRtpSenders()[0];
+			let bobVideoReceiver = bob.getRtpReceivers()[0];
+			let bobAudioSender = bob.getRtpSenders()[0];
+			let bobVideoSender = bob.getRtpSenders()[1];
 
-			t.end();
+			return room.dump()
+				.then((data) =>
+				{
+					t.pass('room.dump() succeeded');
+
+					t.same(data.mapRtpReceiverRtpSenders,
+						{
+							[aliceAudioReceiver._internal.rtpReceiverId] :
+								[
+									String(bobAudioSender._internal.rtpSenderId)
+								],
+							[aliceVideoReceiver._internal.rtpReceiverId] :
+								[
+									String(bobVideoSender._internal.rtpSenderId)
+								],
+							[bobVideoReceiver._internal.rtpReceiverId] :
+								[
+									String(aliceVideoSender._internal.rtpSenderId)
+								]
+						},
+						'`data.mapRtpReceiverRtpSenders` match the expected values');
+				})
+				.catch((error) => t.fail(`room.dump() failed: ${error}`));
+		})
+		.then(() =>
+		{
+			return t.test('close aliceAudioReceiver', { timeout: 1000 }, (t2) =>
+			{
+				t2.plan(2);
+
+				let aliceAudioReceiver = alice.getRtpReceivers()[0];
+				let bobAudioSender = bob.getRtpSenders()[0];
+
+				aliceAudioReceiver.on('close', () => t2.pass('aliceAudioReceiver closed'));
+				bobAudioSender.on('close', () => t2.pass('bobAudioSender closed'));
+
+				aliceAudioReceiver.close();
+			});
+		})
+		.then(() =>
+		{
+			let aliceVideoReceiver = alice.getRtpReceivers()[0];
+			let aliceVideoSender = alice.getRtpSenders()[0];
+			let bobVideoReceiver = bob.getRtpReceivers()[0];
+			let bobVideoSender = bob.getRtpSenders()[0];
+
+			return room.dump()
+				.then((data) =>
+				{
+					t.pass('room.dump() succeeded');
+
+					t.same(data.mapRtpReceiverRtpSenders,
+						{
+							[aliceVideoReceiver._internal.rtpReceiverId] :
+								[
+									String(bobVideoSender._internal.rtpSenderId)
+								],
+							[bobVideoReceiver._internal.rtpReceiverId] :
+								[
+									String(aliceVideoSender._internal.rtpSenderId)
+								]
+						},
+						'`data.mapRtpReceiverRtpSenders` match the expected values');
+				})
+				.catch((error) => t.fail(`room.dump() failed: ${error}`));
+		})
+		.then(() =>
+		{
+			return t.test('close bob', { timeout: 1000 }, (t2) =>
+			{
+				t2.plan(4);
+
+				let aliceVideoReceiver = alice.getRtpReceivers()[0];
+				let aliceVideoSender = alice.getRtpSenders()[0];
+				let bobVideoReceiver = bob.getRtpReceivers()[0];
+				let bobVideoSender = bob.getRtpSenders()[0];
+
+				bob.on('close', () => t2.pass('bob closed'));
+				bobVideoReceiver.on('close', () => t2.pass('bobVideoReceiver closed'));
+				bobVideoSender.on('close', () => t2.pass('bobVideoSender closed'));
+				aliceVideoSender.on('close', () => t2.pass('aliceVideoSender closed'));
+
+				bob.close();
+			});
+		})
+		.then(() =>
+		{
+			t.equal(alice.getRtpReceivers().length, 1, 'alice.getRtpReceivers() must retrieve 1');
+			t.equal(bob.getRtpReceivers().length, 0, 'bob.getRtpReceivers() must retrieve 0');
+			t.equal(alice.getRtpSenders().length, 0, 'alice.getRtpSenders() must retrieve 0');
+			t.equal(bob.getRtpSenders().length, 0, 'bob.getRtpSenders() must retrieve 0');
+
+			let aliceVideoReceiver = alice.getRtpReceivers()[0];
+
+			return room.dump()
+				.then((data) =>
+				{
+					t.pass('room.dump() succeeded');
+
+					t.same(data.mapRtpReceiverRtpSenders,
+						{
+							[aliceVideoReceiver._internal.rtpReceiverId] : []
+						},
+						'`data.mapRtpReceiverRtpSenders` match the expected values');
+
+					t.end();
+				})
+				.catch((error) => t.fail(`room.dump() failed: ${error}`));
 		});
 });
 
