0f2d5743d6a72a05a30143bcd357d2d587aafd41
Iñaki Baz Castillo <ibc@aliax.net>
2016-02-21 03:59:02 +0100

    Re-enable test units

--- gulpfile.js
+++ gulpfile.js
@@ -8,13 +8,13 @@ const shell = require('gulp-shell');
 
 let tests =
 [
-	// 'test/test_mediasoup.js',
-	// 'test/test_Server.js',
-	// 'test/test_Room.js',
-	// 'test/test_Peer.js',
-	// 'test/test_Transport.js',
-	// 'test/test_RtpReceiver.js',
-	// 'test/test_extra.js',
+	'test/test_mediasoup.js',
+	'test/test_Server.js',
+	'test/test_Room.js',
+	'test/test_Peer.js',
+	'test/test_Transport.js',
+	'test/test_RtpReceiver.js',
+	'test/test_extra.js',
 	'test/test_scene_1.js'
 ];
 
