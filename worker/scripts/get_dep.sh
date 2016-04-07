b2add932d2eec2f4f0fbfdf08ba6d201d62986a2
Iñaki Baz Castillo <ibc@aliax.net>
2016-04-07 12:49:39 +0200

    Update libuv to 1.9.0

--- worker/scripts/get_dep.sh
+++ worker/scripts/get_dep.sh
@@ -95,7 +95,7 @@ function get_netstring()
 function get_libuv()
 {
 	GIT_REPO="https://github.com/libuv/libuv.git"
-	GIT_TAG="v1.8.0"
+	GIT_TAG="v1.9.0"
 	DEST="deps/libuv"
 
 	if [ -d "${DEST}" ] ; then
