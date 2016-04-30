cb065ca670167a984a6e63ea267dfbde79541aa7
Iñaki Baz Castillo <ibc@aliax.net>
2016-04-30 13:27:32 +0200

    Refactor TransportTuple json output

--- worker/src/RTC/TransportTuple.cpp
+++ worker/src/RTC/TransportTuple.cpp
@@ -13,39 +13,30 @@ namespace RTC
 	{
 		MS_TRACE();
 
-		static const Json::StaticString k_local("local");
-		static const Json::StaticString k_remote("remote");
-		static const Json::StaticString k_ip("ip");
-		static const Json::StaticString k_port("port");
+		static const Json::StaticString k_localIP("localIP");
+		static const Json::StaticString k_remoteIP("remoteIP");
+		static const Json::StaticString k_localPort("localPort");
+		static const Json::StaticString k_remotePort("remotePort");
 		static const Json::StaticString k_protocol("protocol");
 		static const Json::StaticString v_udp("udp");
 		static const Json::StaticString v_tcp("tcp");
 
 		Json::Value json(Json::objectValue);
-		Json::Value json_local(Json::objectValue);
-		Json::Value json_remote(Json::objectValue);
 		int ip_family;
 		std::string ip;
 		uint16_t port;
 
 		Utils::IP::GetAddressInfo(this->GetLocalAddress(), &ip_family, ip, &port);
-		json_local[k_ip] = ip;
-		json_local[k_port] = (Json::UInt)port;
+		json[k_localIP] = ip;
+		json[k_localPort] = (Json::UInt)port;
 		if (this->GetProtocol() == RTC::TransportTuple::Protocol::UDP)
-			json_local[k_protocol] = v_udp;
+			json[k_protocol] = v_udp;
 		else
-			json_local[k_protocol] = v_tcp;
+			json[k_protocol] = v_tcp;
 
 		Utils::IP::GetAddressInfo(this->GetRemoteAddress(), &ip_family, ip, &port);
-		json_remote[k_ip] = ip;
-		json_remote[k_port] = (Json::UInt)port;
-		if (this->GetProtocol() == RTC::TransportTuple::Protocol::UDP)
-			json_remote[k_protocol] = v_udp;
-		else
-			json_remote[k_protocol] = v_tcp;
-
-		json[k_local] = json_local;
-		json[k_remote] = json_remote;
+		json[k_remoteIP] = ip;
+		json[k_remotePort] = (Json::UInt)port;
 
 		return json;
 	}
