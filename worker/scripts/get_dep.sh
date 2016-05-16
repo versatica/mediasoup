3886e49bcf76f65bba1082f0bb224cc019913ca1
Iñaki Baz Castillo <ibc@aliax.net>
2016-05-17 01:28:03 +0200

    Update libuv to 1.9.1 and jsoncpp to 1.7.2

--- worker/scripts/get_dep.sh
+++ worker/scripts/get_dep.sh
@@ -39,7 +39,7 @@ function get_gyp()
 function get_jsoncpp()
 {
 	GIT_REPO="https://github.com/open-source-parsers/jsoncpp.git"
-	GIT_TAG="1.7.1"
+	GIT_TAG="1.7.2"
 	DEST="deps/jsoncpp/jsoncpp"
 
 	if [ -d "${DEST}" ] ; then
@@ -95,7 +95,7 @@ function get_netstring()
 function get_libuv()
 {
 	GIT_REPO="https://github.com/libuv/libuv.git"
-	GIT_TAG="v1.9.0"
+	GIT_TAG="v1.9.1"
 	DEST="deps/libuv"
 
 	if [ -d "${DEST}" ] ; then
