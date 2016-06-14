1dee506cf13522fd1db86442cce2f55db5cca5ba
Iñaki Baz Castillo <ibc@aliax.net>
2016-06-14 23:01:03 +0200

    Fix wrong "const" usage (fails in GCC)

--- worker/src/RTC/Room.cpp
+++ worker/src/RTC/Room.cpp
@@ -357,7 +357,7 @@ namespace RTC
 		// Set codecs.
 		{
 			// Available dynamic payload types.
-			static const std::vector<const uint8_t> dynamicPayloadTypes
+			static const std::vector<uint8_t> dynamicPayloadTypes
 			{
 				100, 101, 102, 103, 104, 105, 106, 107, 108, 109,
 				110, 111, 112, 113, 114, 115, 116, 117, 118, 119,
