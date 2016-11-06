be70c75711b391694dca63d3ec8b70a6bcb3a8fb
José Luis Millán <jmillan@aliax.net>
2016-11-06 23:49:54 +0100

    Rtcp: add FeedbackRtpSrReq.cpp file

---
+++ worker/src/RTC/RTCP/FeedbackRtpSrReq.cpp
@@ -0,0 +1,38 @@
+#define MS_CLASS "RTC::RTCP::FeedbackRtpSrReqPacket"
+
+#include "RTC/RTCP/FeedbackRtpSrReq.h"
+#include "Logger.h"
+
+namespace RTC { namespace RTCP
+{
+
+/* FeedbackRtpSrReqPacket Class methods. */
+
+	FeedbackRtpSrReqPacket* FeedbackRtpSrReqPacket::Parse(const uint8_t* data, size_t len)
+	{
+		MS_TRACE_STD();
+
+		if (sizeof(CommonHeader) + sizeof(FeedbackPacket::Header) > len)
+		{
+				MS_WARN("not enough space for Feedback packet, discarded");
+				return nullptr;
+		}
+
+		CommonHeader* commonHeader = (CommonHeader*)data;
+		return new FeedbackRtpSrReqPacket(commonHeader);
+	}
+
+	void FeedbackRtpSrReqPacket::Dump()
+	{
+		MS_TRACE_STD();
+
+		if (!Logger::HasDebugLevel())
+			return;
+
+		MS_WARN("\t<FeedbackRtpSrReqPacket>");
+		FeedbackRtpPacket::Dump();
+		MS_WARN("\t</FeedbackRtpSrReqPacket>");
+	}
+
+} } // RTP::RTCP
+
