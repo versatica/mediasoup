26de56ee8d76939d27adcf46dba2b1fff9c4499f
Iñaki Baz Castillo <ibc@aliax.net>
2017-02-20 00:15:09 +0100

    Proper static stuff initialization in worker tests

--- worker/test/test-bitrate.cpp
+++ worker/test/test-bitrate.cpp
@@ -1,12 +1,13 @@
 #include "include/catch.hpp"
 #include "common.h"
+#include "DepLibUV.h"
 #include "RTC/RtpDataCounter.h"
-#include "../include/DepLibUV.h"
 #include <vector>
 
 using namespace RTC;
 
-struct data {
+struct data
+{
 	uint32_t offset;
 	uint32_t size;
 	uint32_t rate;
@@ -17,6 +18,7 @@ void validate(RateCalculator& rate, uint64_t time_base,  std::vector<data>& inpu
 	for (auto it = input.begin(); it != input.end(); ++it)
 	{
 		auto& item = *it;
+
 		rate.Update(item.size, time_base + item.offset);
 		REQUIRE(rate.GetRate(time_base + item.offset) == item.rate);
 	}
@@ -24,14 +26,14 @@ void validate(RateCalculator& rate, uint64_t time_base,  std::vector<data>& inpu
 
 SCENARIO("Bitrate calculator", "[rtp][bitrate]")
 {
-	DepLibUV::ClassInit();
 	uint64_t now = DepLibUV::GetTime();
 
 	SECTION("receive single item per 1000 ms")
 	{
 		RateCalculator rate;
-		std::vector<data> input = {
-			{ 0,    5, 40 }
+		std::vector<data> input =
+		{
+			{ 0, 5, 40 }
 		};
 
 		validate(rate, now, input);
@@ -40,11 +42,12 @@ SCENARIO("Bitrate calculator", "[rtp][bitrate]")
 	SECTION("receive multiple items per 1000 ms")
 	{
 		RateCalculator rate;
-		std::vector<data> input = {
-			{ 0,    5, 40  },
-			{ 100,  2, 56  },
-			{ 300,  2, 72  },
-			{ 999,  4, 104 }
+		std::vector<data> input =
+		{
+			{ 0,   5, 40  },
+			{ 100, 2, 56  },
+			{ 300, 2, 72  },
+			{ 999, 4, 104 }
 		};
 
 		validate(rate, now, input);
@@ -53,7 +56,8 @@ SCENARIO("Bitrate calculator", "[rtp][bitrate]")
 	SECTION("slide")
 	{
 		RateCalculator rate;
-		std::vector<data> input = {
+		std::vector<data> input =
+		{
 			{ 0,    5, 40 },
 			{ 999,  2, 56 },
 			{ 1001, 1, 24 },
@@ -65,6 +69,4 @@ SCENARIO("Bitrate calculator", "[rtp][bitrate]")
 
 		REQUIRE(rate.GetRate(now + 3000) == 0);
 	}
-
-	DepLibUV::ClassDestroy();
 }
--- worker/test/tests.cpp
+++ worker/test/tests.cpp
@@ -3,6 +3,12 @@
 #include "include/catch.hpp"
 #include "Settings.h"
 #include "LogLevel.h"
+#include "DepLibUV.h"
+#include "DepOpenSSL.h"
+#include "Utils.h"
+
+static void init();
+static void destroy();
 
 int main(int argc, char* argv[])
 {
@@ -10,7 +16,27 @@ int main(int argc, char* argv[])
 	Settings::configuration.logTags.rtp = true;
 	Settings::configuration.logTags.rtcp = true;
 
+	init();
+
 	int ret = Catch::Session().run(argc, argv);
 
+	destroy();
+
 	return ret;
+}
+
+void init()
+{
+	// Initialize static stuff.
+	DepLibUV::ClassInit();
+	DepOpenSSL::ClassInit();
+	Utils::Crypto::ClassInit();
+}
+
+void destroy()
+{
+	// Free static stuff.
+	Utils::Crypto::ClassDestroy();
+	DepOpenSSL::ClassDestroy();
+	DepLibUV::ClassDestroy();
 }
