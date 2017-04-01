24e37d366429c62633fc70eecbf9d8f17b2c909b
Iñaki Baz Castillo <ibc@aliax.net>
2017-04-01 19:12:02 +0200

    bar

--- worker/src/RTC/RtpStream.cpp
+++ worker/src/RTC/RtpStream.cpp
@@ -43,11 +43,13 @@ namespace RTC
 		}
 
 		// If not a valid packet ignore it.
-		if (!UpdateSeq(seq))
+		if (!UpdateSeq(packet))
 		{
 			if (!this->probation)
 			{
-				MS_WARN_TAG(rtp, "invalid packet [ssrc:%" PRIu32 ", seq:%" PRIu16 "]", packet->GetSsrc(), packet->GetSequenceNumber());
+				MS_WARN_TAG(rtp,
+					"invalid packet [ssrc:%" PRIu32 ", seq:%" PRIu16 "]",
+					packet->GetSsrc(), packet->GetSequenceNumber());
 			}
 
 			return false;
@@ -83,10 +85,11 @@ namespace RTC
 		onInitSeq();
 	}
 
-	bool RtpStream::UpdateSeq(uint16_t seq)
+	bool RtpStream::UpdateSeq(RTC::RtpPacket* packet)
 	{
 		MS_TRACE();
 
+		uint16_t seq = packet->GetSequenceNumber();
 		uint16_t udelta = seq - this->max_seq;
 
 		/*
@@ -138,10 +141,18 @@ namespace RTC
 				 * restarted without telling us so just re-sync
 				 * (i.e., pretend this was the first packet).
 				 */
+				MS_WARN_TAG(rtp,
+					"too bad sequence number, re-syncing RTP [ssrc:%" PRIu32 ", seq:%" PRIu16 "]",
+					packet->GetSsrc(), packet->GetSequenceNumber());
+
 				InitSeq(seq);
 			}
 			else
 			{
+				MS_WARN_TAG(rtp,
+					"bad sequence number, ignoring packet [ssrc:%" PRIu32 ", seq:%" PRIu16 "]",
+					packet->GetSsrc(), packet->GetSequenceNumber());
+
 				this->bad_seq = (seq + 1) & (RTP_SEQ_MOD - 1);
 
 				return false;
