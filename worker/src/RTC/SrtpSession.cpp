21113949198470595d809ad1f9a33a22d5c5cb2e
Iñaki Baz Castillo <ibc@aliax.net>
2017-09-07 17:13:14 +0200

    libsrtp: use policy.window_size = 1024;

--- worker/src/RTC/SrtpSession.cpp
+++ worker/src/RTC/SrtpSession.cpp
@@ -92,7 +92,7 @@ namespace RTC
 		policy.key        = key;
 		// Required for sending RTP retransmission without RTX.
 		policy.allow_repeat_tx = 1;
-		policy.window_size     = 128;
+		policy.window_size     = 1024;
 		policy.next            = nullptr;
 
 		// Set the SRTP session.
