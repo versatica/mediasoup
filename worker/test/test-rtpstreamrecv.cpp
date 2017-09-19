b66b6c02cfb5cafa020cc9f1e6027b46cecb8b05
Iñaki Baz Castillo <ibc@aliax.net>
2017-09-19 21:12:37 +0200

    Fix error in test-rtpstreamrecv

--- worker/test/test-rtpstreamrecv.cpp
+++ worker/test/test-rtpstreamrecv.cpp
@@ -36,7 +36,7 @@ SCENARIO("receive RTP packets and trigger NACK", "[rtp][rtpstream]")
 			this->seqNumbers = seqNumbers;
 		}
 
-		virtual void OnRtpStreamRecvPliRequired(RtpStreamRecv* rtpStream, RTC::RtpEncodingParameters::Profile profile) override
+		virtual void OnRtpStreamRecvPliRequired(RtpStreamRecv* rtpStream) override
 		{
 			INFO("PLI required");
 
