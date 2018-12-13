25c216b26894148e1522c9f49a36ba1d3cf92b86
Iñaki Baz Castillo <ibc@aliax.net>
2018-12-13 04:40:06 +0100

    RtpPacket.cpp: fix wrong memory access (fuzzer all things!!!)

--- worker/src/RTC/RtpPacket.cpp
+++ worker/src/RTC/RtpPacket.cpp
@@ -560,7 +560,7 @@ namespace RTC
 			uint8_t* extensionEnd   = extensionStart + GetExtensionHeaderLength();
 			uint8_t* ptr            = extensionStart;
 
-			while (ptr < extensionEnd)
+			while (ptr + sizeof(size_t) < extensionEnd)
 			{
 				uint8_t id = *ptr;
 				size_t len = *(++ptr);
