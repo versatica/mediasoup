343037df13d57d49c2b1680eddc2046df1f55448
Iñaki Baz Castillo <ibc@aliax.net>
2018-12-17 03:40:14 +0100

    fuzzer: use packet methods in fuzzRtp() and fuzzStun()

--- worker/fuzzer/src/fuzzRtp.cpp
+++ worker/fuzzer/src/fuzzRtp.cpp
@@ -15,7 +15,51 @@ namespace fuzzers
 		if (!packet)
 			return;
 
-		// TODO: Add more checks here.
+		// packet->Dump(); // NOTE: This prints to stdout.
+		packet->GetData();
+		packet->GetSize();
+		packet->GetPayloadType();
+		packet->SetPayloadType(100);
+		packet->HasMarker();
+		packet->SetMarker(true);
+		packet->SetPayloadPaddingFlag(true);
+		packet->GetSequenceNumber();
+		packet->SetSequenceNumber(12345);
+		packet->GetTimestamp();
+		packet->SetTimestamp(8888);
+		packet->GetSsrc();
+		packet->SetSsrc(666);
+		packet->HasExtensionHeader();
+		packet->GetExtensionHeaderId();
+		packet->GetExtensionHeaderLength();
+		packet->GetExtensionHeaderValue();
+		// TODO: packet->MangleExtensionHeaderIds();
+		packet->HasOneByteExtensions();
+		packet->HasTwoBytesExtensions();
+		// TODO: packet->AddExtensionMapping();
+		// TODO: packet->GetExtension();
+		// TODO: packet->ReadAudioLevel();
+		// TODO: packet->ReadAbsSendTime();
+		// TODO: packet->ReadMid();
+		// TODO: packet->ReadRid();
+		packet->GetPayload();
+		packet->GetPayloadLength();
+		packet->GetPayloadPadding();
+		packet->IsKeyFrame();
+
+		{
+			uint8_t buffer[len];
+			auto* clonedPacket = packet->Clone(buffer);
+
+			delete clonedPacket;
+		}
+
+		// TODO: packet->RtxEncode(); // This cannot be tested this way.
+		// TODO: packet->RtxDecode(); // This cannot be tested this way.
+		// TODO: packet->SetPayloadDescriptorHandler();
+		// TODO: packet->EncodePayload();
+		// TODO: packet->EncodePayload();
+		// TODO: packet->ShiftPayload();
 
 		delete packet;
 	}
--- worker/fuzzer/src/fuzzStun.cpp
+++ worker/fuzzer/src/fuzzStun.cpp
@@ -15,7 +15,35 @@ namespace fuzzers
 		if (!msg)
 			return;
 
-		// TODO: Add more checks here.
+		// msg->Dump(); // NOTE: This prints to stdout.
+		msg->GetClass();
+		msg->GetMethod();
+		msg->GetData();
+		msg->GetSize();
+		msg->SetUsername("foo", 3);
+		msg->SetPriority(123);
+		msg->SetIceControlling(123);
+		msg->SetIceControlled(123);
+		msg->SetUseCandidate();
+		// TODO: msg->SetXorMappedAddress();
+		msg->SetErrorCode(666);
+		// TODO: msg->SetMessageIntegrity();
+		msg->SetFingerprint();
+		msg->GetUsername();
+		msg->GetPriority();
+		msg->GetIceControlling();
+		msg->GetIceControlled();
+		msg->HasUseCandidate();
+		msg->GetErrorCode();
+		msg->HasMessageIntegrity();
+		msg->HasFingerprint();
+		msg->CheckAuthentication("foo", "bar");
+		// TODO: msg->CreateSuccessResponse(); // This cannot be easily tested.
+		// TODO: msg->CreateErrorResponse(); // This cannot be easily tested.
+		msg->Authenticate("lalala");
+		// TODO: Cannot test Serialize() because we don't know the exact required
+		// buffer size (setters above may change the total size).
+		// TODO: msg->Serialize();
 
 		delete msg;
 	}
