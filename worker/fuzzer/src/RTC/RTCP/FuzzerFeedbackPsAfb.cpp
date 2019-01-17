d9107418679f87e1e92e096c04c671aee7374d80
José Luis Millán <jmillan@aliax.net>
2019-01-17 14:38:40 -0500

    fuzzer: RTCP

---
+++ worker/fuzzer/src/RTC/RTCP/FuzzerFeedbackPsAfb.cpp
@@ -0,0 +1,10 @@
+#include "RTC/RTCP/FuzzerFeedbackPsAfb.hpp"
+
+void Fuzzer::RTC::RTCP::FeedbackPsAfb::Fuzz(::RTC::RTCP::FeedbackPsAfbPacket* packet)
+{
+	packet->Dump();
+	packet->Serialize(::RTC::RTCP::Buffer);
+	packet->GetSize();
+
+	packet->GetApplication();
+}
