4f4d95b26855e54a2598ddddae66463f4da57333
Iñaki Baz Castillo <ibc@aliax.net>
2019-02-08 02:21:08 +0100

    tests.cpp: Add MS_TEST_LOG_LEVEL env variable to set worker logLevel

--- worker/test/src/tests.cpp
+++ worker/test/src/tests.cpp
@@ -6,10 +6,24 @@
 #include "Settings.hpp"
 #include "Utils.hpp"
 #include "catch.hpp"
+#include <cstdlib> // std::getenv()
 
 int main(int argc, char* argv[])
 {
-	Settings::configuration.logLevel = LogLevel::LOG_NONE;
+	LogLevel logLevel{ LogLevel::LOG_NONE };
+
+	// Get logLevel from ENV variable.
+	if (std::getenv("MS_TEST_LOG_LEVEL"))
+	{
+		if (std::string(std::getenv("MS_TEST_LOG_LEVEL")) == "debug")
+			logLevel = LogLevel::LOG_DEBUG;
+		else if (std::string(std::getenv("MS_TEST_LOG_LEVEL")) == "warn")
+			logLevel = LogLevel::LOG_WARN;
+		else if (std::string(std::getenv("MS_TEST_LOG_LEVEL")) == "error")
+			logLevel = LogLevel::LOG_ERROR;
+	}
+
+	Settings::configuration.logLevel = logLevel;
 
 	// Initialize static stuff.
 	DepLibUV::ClassInit();
