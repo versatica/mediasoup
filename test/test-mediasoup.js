97c618ac7bf0adee96611e9851bca5fa8e9789b3
Iñaki Baz Castillo <ibc@aliax.net>
2019-04-15 21:49:04 +0200

    Add mediasoup.parseScalabilityMode() API

--- test/test-mediasoup.js
+++ test/test-mediasoup.js
@@ -1,7 +1,11 @@
 const { toBeType } = require('jest-tobetype');
 const pkg = require('../package.json');
 const mediasoup = require('../');
-const { version, getSupportedRtpCapabilities } = mediasoup;
+const {
+	version,
+	getSupportedRtpCapabilities,
+	parseScalabilityMode
+} = mediasoup;
 
 expect.extend({ toBeType });
 
@@ -9,7 +13,7 @@ test('mediasoup.version exposes the package version', () =>
 {
 	expect(version).toBeType('string');
 	expect(version).toBe(pkg.version);
-}, 2000);
+}, 500);
 
 test('mediasoup.getSupportedRtpCapabilities() returns the mediasoup RTP capabilities', () =>
 {
@@ -24,4 +28,12 @@ test('mediasoup.getSupportedRtpCapabilities() returns the mediasoup RTP capabili
 	const rtpCapabilities2 = getSupportedRtpCapabilities();
 
 	expect(rtpCapabilities2).not.toEqual(rtpCapabilities);
-}, 2000);
+}, 500);
+
+test('mediasoup.parseScalabilityMode() works', () =>
+{
+	expect(parseScalabilityMode('L1T3')).toEqual({ spatialLayers: 1, temporalLayers: 3 });
+	expect(parseScalabilityMode('L3T2_KEY')).toEqual({ spatialLayers: 3, temporalLayers: 2 });
+	expect(parseScalabilityMode('foo')).toEqual({ spatialLayers: 1, temporalLayers: 1 });
+	expect(parseScalabilityMode(null)).toEqual({ spatialLayers: 1, temporalLayers: 1 });
+}, 500);
