c1736204c44a169b5ebce1b4a7a0431cdcb33e80
Iñaki Baz Castillo <ibc@aliax.net>
2019-04-15 18:02:39 +0200

    Update libuv to 1.28.0

--- worker/scripts/get-dep.sh
+++ worker/scripts/get-dep.sh
@@ -76,7 +76,7 @@ function get_netstring()
 function get_libuv()
 {
 	GIT_REPO="https://github.com/libuv/libuv.git"
-	GIT_TAG="v1.27.0"
+	GIT_TAG="v1.28.0"
 	DEST="deps/libuv"
 
 	get_dep "${GIT_REPO}" "${GIT_TAG}" "${DEST}"
