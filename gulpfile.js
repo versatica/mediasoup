4f10db0d1fbd8c8247118f44c488d9678f219b39
HaiYang Wu <wuhaiyang1213@gmail.com>
2019-09-02 10:11:02 +0800

    use run-script-os instead of gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,4 @@
-const os = require('os');
 const gulp = require('gulp');
-const shell = require('gulp-shell');
 const clangFormat = require('gulp-clang-format');
 
 const workerFiles =
@@ -39,39 +37,3 @@ gulp.task('format:worker', () =>
 		.pipe(clangFormat.format('file'))
 		.pipe(gulp.dest('.'));
 });
-
-gulp.task('win:build', shell.task(
-	[
-		'echo build for windows',
-		'python ./worker/scripts/configure.py --format=msvs',
-		`MSBuild ./worker/mediasoup-worker.sln /p:Configuration=${process.env.MEDIASOUP_BUILDTYPE === 'Debug' ?'Debug' : 'Release'} -t:mediasoup-worker `
-	],
-	{
-		verbose : true
-	}
-));
-
-gulp.task('make:build', shell.task(
-	[
-		'make -C worker'
-	],
-	{
-		verbose : true
-	}
-));
-
-gulp.task('build', gulp.series(os.platform() === 'win32'? 'win:build' : 'make:build'));
-
-gulp.task('test:win:worker', shell.task(
-	[
-		'echo not support yet!!! run test in visual studio!!!'
-	]
-));
-
-gulp.task('test:make:worker', shell.task(
-	[
-		'make test -C worker'
-	]
-));
-
-gulp.task('test:worker', gulp.series(os.platform() === 'win32'? 'test:win:worker' : 'test:make:worker'));
