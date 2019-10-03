461e7a518fc2adef463800b80b16db230eedb533
HaiYang Wu <wuhaiyang1213@gmail.com>
2019-10-04 01:48:48 +0800

    include

--- worker/include/common.hpp
+++ worker/include/common.hpp
@@ -1,7 +1,12 @@
 #ifndef MS_COMMON_HPP
 #define MS_COMMON_HPP
 
-#include <algorithm> // std::transform(), std::find(), std::min(), std::max()
+#include <algorithm>  // std::transform(), std::find(), std::min(), std::max()
+#include <cinttypes>  // PRIu64, etc
+#include <cstddef>    // size_t
+#include <cstdint>    // uint8_t, etc
+#include <functional> // std::function
+#include <memory>     // std::addressof()
 #ifdef _WIN32
 #include <winsock2.h>
 // https://stackoverflow.com/a/27443191/2085408
@@ -17,17 +22,8 @@ typedef SSIZE_T ssize_t;
 #endif
 #else
 #include <arpa/inet.h>  // htonl(), htons(), ntohl(), ntohs()
-#include <cinttypes>    // PRIu64, etc
-#include <cstddef>      // size_t
-#include <cstdint>      // uint8_t, etc
-#include <functional>   // std::function
-#include <memory>       // std::addressof()
 #include <netinet/in.h> // sockaddr_in, sockaddr_in6
 #include <sys/socket.h> // struct sockaddr, struct sockaddr_storage, AF_INET, AF_INET6
 #endif
-#include <cinttypes> // PRIu64, etc
-#include <cstddef>   // size_t
-#include <cstdint>   // uint8_t, etc
-#include <memory>    // std::addressof()
 
 #endif
