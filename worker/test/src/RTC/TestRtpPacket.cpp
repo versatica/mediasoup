017c965b7a50ea03c440cd2496ce02795024d21b
Iñaki Baz Castillo <ibc@aliax.net>
2019-10-06 15:25:53 +0200

    Fix TestRtpPacket.cpp test

--- worker/test/src/RTC/TestRtpPacket.cpp
+++ worker/test/src/RTC/TestRtpPacket.cpp
@@ -458,17 +458,18 @@ SCENARIO("parse RTP packets", "[parser][rtp]")
 		REQUIRE(payload[6] == 0x06);
 		REQUIRE(payload[7] == 0x07);
 
+		// NOTE: This will require padding to 4 bytes.
 		packet->SetPayloadLength(14);
 
-		REQUIRE(packet->GetPayloadLength() == 14);
+		REQUIRE(packet->GetPayloadLength() == 16);
 		REQUIRE(packet->GetPayloadPadding() == 0);
-		REQUIRE(packet->GetSize() == 42);
+		REQUIRE(packet->GetSize() == 44);
 
 		packet->ShiftPayload(4, 4, true);
 
-		REQUIRE(packet->GetPayloadLength() == 18);
+		REQUIRE(packet->GetPayloadLength() == 20);
 		REQUIRE(packet->GetPayloadPadding() == 0);
-		REQUIRE(packet->GetSize() == 46);
+		REQUIRE(packet->GetSize() == 48);
 		REQUIRE(payload[0] == 0x00);
 		REQUIRE(payload[1] == 0x01);
 		REQUIRE(payload[2] == 0x02);
