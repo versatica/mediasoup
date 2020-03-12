00429b13d5c577d43fe7c601eb1d015e602022cd
Iñaki Baz Castillo <ibc@aliax.net>
2020-03-12 15:04:58 +0100

    worker/src/handles/UdpSocket.cpp: use uv__udp_recvmmsg() internally

--- worker/src/handles/UdpSocket.cpp
+++ worker/src/handles/UdpSocket.cpp
@@ -9,7 +9,9 @@
 
 /* Static. */
 
-static constexpr size_t ReadBufferSize{ 65536 };
+// This value will make libuv to use uv__udp_recvmmsg() internally, which is
+// more efficient.
+static constexpr size_t ReadBufferSize{ 65536 * 2 };
 static uint8_t ReadBuffer[ReadBufferSize];
 
 /* Static methods for UV callbacks. */
