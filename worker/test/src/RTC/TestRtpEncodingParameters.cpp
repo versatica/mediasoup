d04e1a932e8504c4c1172fd4b3024638024d6769
José Luis Millán <jmillan@aliax.net>
2020-04-28 16:25:57 +0200

    lint

--- worker/test/src/RTC/TestRtpEncodingParameters.cpp
+++ worker/test/src/RTC/TestRtpEncodingParameters.cpp
@@ -3,15 +3,17 @@
 #include <regex>
 
 static const std::regex ScalabilityModeRegex(
-		"^[LS]([1-9]\\d{0,1})T([1-9]\\d{0,1})(_KEY)?.*", std::regex_constants::ECMAScript);
+  "^[LS]([1-9]\\d{0,1})T([1-9]\\d{0,1})(_KEY)?.*", std::regex_constants::ECMAScript);
 
-struct scalabilityMode {
-	uint8_t spatialLayers = 1;
+struct scalabilityMode
+{
+	uint8_t spatialLayers  = 1;
 	uint8_t temporalLayers = 1;
 	bool ksvc;
 };
 
-static struct scalabilityMode parseScalabilityMode(const std::string& scalabilityMode) {
+static struct scalabilityMode parseScalabilityMode(const std::string& scalabilityMode)
+{
 	struct scalabilityMode result;
 	std::smatch match;
 
@@ -37,82 +39,82 @@ SCENARIO("parseScalabilityMode", "[rtc]")
 {
 	SECTION("parse L1T3")
 	{
-			const auto scalabilityMode = parseScalabilityMode("L1T3");
+		const auto scalabilityMode = parseScalabilityMode("L1T3");
 
-			REQUIRE(scalabilityMode.spatialLayers == 1);
-			REQUIRE(scalabilityMode.temporalLayers == 3);
-			REQUIRE(scalabilityMode.ksvc == false);
+		REQUIRE(scalabilityMode.spatialLayers == 1);
+		REQUIRE(scalabilityMode.temporalLayers == 3);
+		REQUIRE(scalabilityMode.ksvc == false);
 	}
 
 	SECTION("parse L3T2_KEY")
 	{
-			const auto scalabilityMode = parseScalabilityMode("L3T2_KEY");
+		const auto scalabilityMode = parseScalabilityMode("L3T2_KEY");
 
-			REQUIRE(scalabilityMode.spatialLayers == 3);
-			REQUIRE(scalabilityMode.temporalLayers == 2);
-			REQUIRE(scalabilityMode.ksvc == true);
+		REQUIRE(scalabilityMode.spatialLayers == 3);
+		REQUIRE(scalabilityMode.temporalLayers == 2);
+		REQUIRE(scalabilityMode.ksvc == true);
 	}
 
 	SECTION("parse S2T3")
 	{
-			const auto scalabilityMode = parseScalabilityMode("S2T3");
+		const auto scalabilityMode = parseScalabilityMode("S2T3");
 
-			REQUIRE(scalabilityMode.spatialLayers == 2);
-			REQUIRE(scalabilityMode.temporalLayers == 3);
-			REQUIRE(scalabilityMode.ksvc == false);
+		REQUIRE(scalabilityMode.spatialLayers == 2);
+		REQUIRE(scalabilityMode.temporalLayers == 3);
+		REQUIRE(scalabilityMode.ksvc == false);
 	}
 
 	SECTION("parse foo")
 	{
-			const auto scalabilityMode = parseScalabilityMode("foo");
+		const auto scalabilityMode = parseScalabilityMode("foo");
 
-			REQUIRE(scalabilityMode.spatialLayers == 1);
-			REQUIRE(scalabilityMode.temporalLayers == 1);
-			REQUIRE(scalabilityMode.ksvc == false);
+		REQUIRE(scalabilityMode.spatialLayers == 1);
+		REQUIRE(scalabilityMode.temporalLayers == 1);
+		REQUIRE(scalabilityMode.ksvc == false);
 	}
 
 	SECTION("parse ''")
 	{
-			const auto scalabilityMode = parseScalabilityMode("");
+		const auto scalabilityMode = parseScalabilityMode("");
 
-			REQUIRE(scalabilityMode.spatialLayers == 1);
-			REQUIRE(scalabilityMode.temporalLayers == 1);
-			REQUIRE(scalabilityMode.ksvc == false);
+		REQUIRE(scalabilityMode.spatialLayers == 1);
+		REQUIRE(scalabilityMode.temporalLayers == 1);
+		REQUIRE(scalabilityMode.ksvc == false);
 	}
 
 	SECTION("parse S0T3")
 	{
-			const auto scalabilityMode = parseScalabilityMode("S0T3");
+		const auto scalabilityMode = parseScalabilityMode("S0T3");
 
-			REQUIRE(scalabilityMode.spatialLayers == 1);
-			REQUIRE(scalabilityMode.temporalLayers == 1);
-			REQUIRE(scalabilityMode.ksvc == false);
+		REQUIRE(scalabilityMode.spatialLayers == 1);
+		REQUIRE(scalabilityMode.temporalLayers == 1);
+		REQUIRE(scalabilityMode.ksvc == false);
 	}
 
 	SECTION("parse S1T0")
 	{
-			const auto scalabilityMode = parseScalabilityMode("S1T0");
+		const auto scalabilityMode = parseScalabilityMode("S1T0");
 
-			REQUIRE(scalabilityMode.spatialLayers == 1);
-			REQUIRE(scalabilityMode.temporalLayers == 1);
-			REQUIRE(scalabilityMode.ksvc == false);
+		REQUIRE(scalabilityMode.spatialLayers == 1);
+		REQUIRE(scalabilityMode.temporalLayers == 1);
+		REQUIRE(scalabilityMode.ksvc == false);
 	}
 
 	SECTION("parse S20T3")
 	{
-			const auto scalabilityMode = parseScalabilityMode("S20T3");
+		const auto scalabilityMode = parseScalabilityMode("S20T3");
 
-			REQUIRE(scalabilityMode.spatialLayers == 20);
-			REQUIRE(scalabilityMode.temporalLayers == 3);
-			REQUIRE(scalabilityMode.ksvc == false);
+		REQUIRE(scalabilityMode.spatialLayers == 20);
+		REQUIRE(scalabilityMode.temporalLayers == 3);
+		REQUIRE(scalabilityMode.ksvc == false);
 	}
 
 	SECTION("parse S200T3")
 	{
-			const auto scalabilityMode = parseScalabilityMode("S200T3");
+		const auto scalabilityMode = parseScalabilityMode("S200T3");
 
-			REQUIRE(scalabilityMode.spatialLayers == 1);
-			REQUIRE(scalabilityMode.temporalLayers == 1);
-			REQUIRE(scalabilityMode.ksvc == false);
+		REQUIRE(scalabilityMode.spatialLayers == 1);
+		REQUIRE(scalabilityMode.temporalLayers == 1);
+		REQUIRE(scalabilityMode.ksvc == false);
 	}
 }
