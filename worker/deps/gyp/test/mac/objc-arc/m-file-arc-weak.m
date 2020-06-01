66bb7dccf82472e8f4506f22a87ec3f3fb6aa101
Iñaki Baz Castillo <ibc@aliax.net>
2020-06-01 13:20:32 +0200

    Update gyp

--- worker/deps/gyp/test/mac/objc-arc/m-file-arc-weak.m
+++ worker/deps/gyp/test/mac/objc-arc/m-file-arc-weak.m
@@ -2,8 +2,8 @@
 #error "ObjC files without CLANG_ENABLE_OBJC_ARC should not be ARC'd!"
 #endif
 
-#if __has_feature(objc_arc_weak)
-#error "Without CLANG_ENABLE_OBJC_WEAK, weak references should be disabled."
+#if !__has_feature(objc_arc_weak)
+#error "With CLANG_ENABLE_OBJC_WEAK, weak references should be enabled."
 #endif
 
 void m_fun() {}
