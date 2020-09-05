7b65ca09243ae2979d07888be43538bf0f7e32ac
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2020-09-05 20:55:12 +0300

    Refactor macro for request/response handling one more time

--- rust/src/messages.rs
+++ rust/src/messages.rs
@@ -12,55 +12,42 @@ pub(crate) trait Request: Debug + Serialize {
 
 macro_rules! request_response {
     (
-        $request_name: ident,
         $method: literal,
-        $request_impl: tt,
-        $response_name: ident,
-        $response_impl: tt,
+        $request_struct_name: ident $request_struct_impl: tt,
+        $existing_response_type: ty $(,)?
     ) => {
         #[derive(Debug, Serialize)]
-        pub(crate) struct $request_name $request_impl
+        pub(crate) struct $request_struct_name $request_struct_impl
+
+        impl Request for $request_struct_name {
+            type Response = $existing_response_type;
+
+            fn as_method(&self) -> &'static str {
+                $method
+            }
+        }
+    };
+    (
+        $method: literal,
+        $request_struct_name: ident $request_struct_impl: tt $(,)?
+    ) => {
+        // Call above macro with unit type as expected response
+        request_response!($method, $request_struct_name $request_struct_impl, ());
+    };
+    (
+        $method: literal,
+        $request_struct_name: ident $request_struct_impl: tt,
+        $response_struct_name: ident $response_struct_impl: tt,
+    ) => {
+        #[derive(Debug, Serialize)]
+        pub(crate) struct $request_struct_name $request_struct_impl
 
         #[derive(Debug, Deserialize)]
         #[serde(rename_all = "camelCase")]
-        pub(crate) struct $response_name $response_impl
+        pub(crate) struct $response_struct_name $response_struct_impl
 
-        impl Request for $request_name {
-            type Response = $response_name;
-
-            fn as_method(&self) -> &'static str {
-                $method
-            }
-        }
-    };
-    (
-        $request_name: ident,
-        $method: literal,
-        $request_impl: tt,
-        $response_name: ident,
-    ) => {
-        #[derive(Debug, Serialize)]
-        pub(crate) struct $request_name $request_impl
-
-        impl Request for $request_name {
-            type Response = $response_name;
-
-            fn as_method(&self) -> &'static str {
-                $method
-            }
-        }
-    };
-    (
-        $request_name: ident,
-        $method: literal,
-        $request_impl: tt,
-        $response_impl: ty,
-    ) => {
-        #[derive(Debug, Serialize)]
-        pub(crate) struct $request_name $request_impl
-
-        impl Request for $request_name {
-            type Response = $response_impl;
+        impl Request for $request_struct_name {
+            type Response = $response_struct_name;
 
             fn as_method(&self) -> &'static str {
                 $method
@@ -69,528 +56,492 @@ macro_rules! request_response {
     };
 }
 
-request_response!(
-    WorkerDumpRequest,
-    "worker.dump",
-    ;,
-    WorkerDump,
-);
+request_response!("worker.dump", WorkerDumpRequest {}, WorkerDump);
 
 request_response!(
-    WorkerGetResourceRequest,
     "worker.getResourceUsage",
-    ;,
+    WorkerGetResourceRequest {},
     WorkerResourceUsage,
 );
 
 request_response!(
-    WorkerUpdateSettingsRequest,
     "worker.updateSettings",
-    {
+    WorkerUpdateSettingsRequest {
         pub(crate) data: WorkerUpdateSettings,
     },
-    (),
 );
 
 request_response!(
-    WorkerCreateRouterRequest,
     "worker.createRouter",
-    {
+    WorkerCreateRouterRequest {
         pub(crate) internal: RouterInternal,
     },
-    (),
 );
 
 request_response!(
-    RouterCloseRequest,
     "router.close",
-    {
+    RouterCloseRequest {
         pub(crate) internal: RouterInternal,
     },
-    RouterCloseResponse,
-    {
+    RouterCloseResponse {
         // TODO
     },
 );
 
 request_response!(
-    RouterDumpRequest,
     "router.dump",
-    {
+    RouterDumpRequest {
         pub(crate) internal: RouterInternal,
     },
-    RouterDumpResponse,
-    {
+    RouterDumpResponse {
         // TODO
     },
 );
 
 request_response!(
-    RouterCreateWebrtcTransportRequest,
     "router.createWebRtcTransport",
-    {
+    RouterCreateWebrtcTransportRequest {
         pub(crate) internal: TransportInternal,
         pub(crate) data: RouterCreateWebrtcTransportData,
     },
-    RouterCreateWebrtcTransportResponse,
-    {
+    RouterCreateWebrtcTransportResponse {
         // TODO
     },
 );
 
 request_response!(
-    RouterCreatePlainTransportRequest,
     "router.createPlainTransport",
-    {
+    RouterCreatePlainTransportRequest {
         pub(crate) internal: TransportInternal,
         pub(crate) data: RouterCreatePlainTransportData,
     },
-    RouterCreatePlainTransportResponse,
-    {
+    RouterCreatePlainTransportResponse {
         // TODO
     },
 );
 
 request_response!(
-    RouterCreatePipeTransportRequest,
     "router.createPipeTransport",
-    {
+    RouterCreatePipeTransportRequest {
         pub(crate) internal: TransportInternal,
         pub(crate) data: RouterCreatePipeTransportData,
     },
-    RouterCreatePipeTransportResponse,
-    {
+    RouterCreatePipeTransportResponse {
         // TODO
     },
 );
 
 request_response!(
-    RouterCreateDirectTransportRequest,
     "router.createDirectTransport",
-    {
+    RouterCreateDirectTransportRequest {
         pub(crate) internal: TransportInternal,
         pub(crate) data: RouterCreateDirectTransportData,
     },
-    RouterCreateDirectTransportResponse,
-    {
+    RouterCreateDirectTransportResponse {
         // TODO
     },
 );
 
 request_response!(
-    RouterCreateAudioLevelObserverRequest,
     "router.createAudioLevelObserver",
-    {
+    RouterCreateAudioLevelObserverRequest {
         pub(crate) internal: RouterCreateAudioLevelObserverInternal,
         pub(crate) data: RouterCreateAudioLevelObserverData,
     },
-    RouterCreateAudioLevelObserverResponse,
-    {
+    RouterCreateAudioLevelObserverResponse {
         // TODO
     },
 );
 
 request_response!(
-    TransportCloseRequest,
     "transport.close",
-    {
+    TransportCloseRequest {
         pub(crate) internal: TransportInternal,
     },
-    TransportCloseResponse,
-    {
+    TransportCloseResponse {
         // TODO
     },
 );
 
 request_response!(
-    TransportDumpRequest,
     "transport.dump",
-    {
+    TransportDumpRequest {
         pub(crate) internal: TransportInternal,
     },
-    TransportDumpResponse,
-    {
+    TransportDumpResponse {
         // TODO
     },
 );
 
 request_response!(
-    TransportGetStatsRequest,
     "transport.getStats",
-    {
+    TransportGetStatsRequest {
         pub(crate) internal: TransportInternal,
     },
-    TransportGetStatsResponse,
-    {
+    TransportGetStatsResponse {
         // TODO
     },
 );
 
 request_response!(
-    TransportConnectRequest,
     "transport.connect",
-    {
+    TransportConnectRequest {
         pub(crate) internal: TransportInternal,
         pub(crate) data: TransportConnectData,
     },
-    TransportConnectResponse,
-    {
+    TransportConnectResponse {
         // TODO
     },
 );
 
 request_response!(
-    TransportSetMaxIncomingBitrateRequest,
     "transport.setMaxIncomingBitrate",
-    {
+    TransportSetMaxIncomingBitrateRequest {
         pub(crate) internal: TransportInternal,
         pub(crate) data: TransportSetMaxIncomingBitrateData,
     },
-    TransportSetMaxIncomingBitrateResponse,
-    {
+    TransportSetMaxIncomingBitrateResponse {
         // TODO
     },
 );
 
 // TODO: Detail remaining methods, I got bored for now
 request_response!(
-    TransportRestartIceRequest,
     "transport.restartIce",
-    {
+    TransportRestartIceRequest {
         pub(crate) internal: TransportInternal,
     },
-    TransportRestartIceResponse,
-    {
+    TransportRestartIceResponse {
         // TODO
     },
 );
 
-request_response!(
-    TransportProduceRequest,
-    "transport.produce",
-    ;,
-    TransportProduceResponse,
-    {
-        // TODO
-    },
-);
-
-request_response!(
-    TransportConsumeRequest,
-    "transport.consume",
-    ;,
-    TransportConsumeResponse,
-    {
-        // TODO
-    },
-);
-
-request_response!(
-    TransportProduceDataRequest,
-    "transport.produceData",
-    ;,
-    TransportProduceDataResponse,
-    {
-        // TODO
-    },
-);
-
-request_response!(
-    TransportConsumeDataRequest,
-    "transport.consumeData",
-    ;,
-    TransportConsumeDataResponse,
-    {
-        // TODO
-    },
-);
-
-request_response!(
-    TransportEnableTraceEventRequest,
-    "transport.enableTraceEvent",
-    ;,
-    TransportEnableTraceEventResponse,
-    {
-        // TODO
-    },
-);
-
-request_response!(
-    ProducerCloseRequest,
-    "producer.close",
-    ;,
-    ProducerCloseResponse,
-    {
-        // TODO
-    },
-);
-
-request_response!(
-    ProducerDumpRequest,
-    "producer.dump",
-    ;,
-    ProducerDumpResponse,
-    {
-        // TODO
-    },
-);
-
-request_response!(
-    ProducerGetStatsRequest,
-    "producer.getStats",
-    ;,
-    ProducerGetStatsResponse,
-    {
-        // TODO
-    },
-);
-
-request_response!(
-    ProducerPauseRequest,
-    "producer.pause",
-    ;,
-    ProducerPauseResponse,
-    {
-        // TODO
-    },
-);
-
-request_response!(
-    ProducerResumeRequest,
-    "producer.resume",
-    ;,
-    ProducerResumeResponse,
-    {
-        // TODO
-    },
-);
-
-request_response!(
-    ProducerEnableTraceEventRequest,
-    "producer.enableTraceEvent",
-    ;,
-    ProducerEnableTraceEventResponse,
-    {
-        // TODO
-    },
-);
-
-request_response!(
-    ConsumerCloseRequest,
-    "consumer.close",
-    ;,
-    ConsumerCloseResponse,
-    {
-        // TODO
-    },
-);
-
-request_response!(
-    ConsumerDumpRequest,
-    "consumer.dump",
-    ;,
-    ConsumerDumpResponse,
-    {
-        // TODO
-    },
-);
-
-request_response!(
-    ConsumerGetStatsRequest,
-    "consumer.getStats",
-    ;,
-    ConsumerGetStatsResponse,
-    {
-        // TODO
-    },
-);
-
-request_response!(
-    ConsumerPauseRequest,
-    "consumer.pause",
-    ;,
-    ConsumerPauseResponse,
-    {
-        // TODO
-    },
-);
-
-request_response!(
-    ConsumerResumeRequest,
-    "consumer.resume",
-    ;,
-    ConsumerResumeResponse,
-    {
-        // TODO
-    },
-);
-
-request_response!(
-    ConsumerSetPreferredLayersRequest,
-    "consumer.setPreferredLayers",
-    ;,
-    ConsumerSetPreferredLayersResponse,
-    {
-        // TODO
-    },
-);
-
-request_response!(
-    ConsumerSetPriorityRequest,
-    "consumer.setPriority",
-    ;,
-    ConsumerSetPriorityResponse,
-    {
-        // TODO
-    },
-);
-
-request_response!(
-    ConsumerRequestKeyFrameRequest,
-    "consumer.requestKeyFrame",
-    ;,
-    ConsumerRequestKeyFrameResponse,
-    {
-        // TODO
-    },
-);
-
-request_response!(
-    ConsumerEnableTraceEventRequest,
-    "consumer.enableTraceEvent",
-    ;,
-    ConsumerEnableTraceEventResponse,
-    {
-        // TODO
-    },
-);
-
-request_response!(
-    DataProducerCloseRequest,
-    "dataProducer.close",
-    ;,
-    DataProducerCloseResponse,
-    {
-        // TODO
-    },
-);
-
-request_response!(
-    DataProducerDumpRequest,
-    "dataProducer.dump",
-    ;,
-    DataProducerDumpResponse,
-    {
-        // TODO
-    },
-);
-
-request_response!(
-    DataProducerGetStatsRequest,
-    "dataProducer.getStats",
-    ;,
-    DataProducerGetStatsResponse,
-    {
-        // TODO
-    },
-);
-
-request_response!(
-    DataConsumerCloseRequest,
-    "dataConsumer.close",
-    ;,
-    DataConsumerCloseResponse,
-    {
-        // TODO
-    },
-);
-
-request_response!(
-    DataConsumerDumpRequest,
-    "dataConsumer.dump",
-    ;,
-    DataConsumerDumpResponse,
-    {
-        // TODO
-    },
-);
-
-request_response!(
-    DataConsumerGetStatsRequest,
-    "dataConsumer.getStats",
-    ;,
-    DataConsumerGetStatsResponse,
-    {
-        // TODO
-    },
-);
-
-request_response!(
-    DataConsumerGetBufferedAmountRequest,
-    "dataConsumer.getBufferedAmount",
-    ;,
-    DataConsumerGetBufferedAmountResponse,
-    {
-        // TODO
-    },
-);
-
-request_response!(
-    DataConsumerSetBufferedAmountLowThresholdRequest,
-    "dataConsumer.setBufferedAmountLowThreshold",
-    ;,
-    DataConsumerSetBufferedAmountLowThresholdResponse,
-    {
-        // TODO
-    },
-);
-
-request_response!(
-    RtpObserverCloseRequest,
-    "rtpObserver.close",
-    ;,
-    RtpObserverCloseResponse,
-    {
-        // TODO
-    },
-);
-
-request_response!(
-    RtpObserverPauseRequest,
-    "rtpObserver.pause",
-    ;,
-    RtpObserverPauseResponse,
-    {
-        // TODO
-    },
-);
-
-request_response!(
-    RtpObserverResumeRequest,
-    "rtpObserver.resume",
-    ;,
-    RtpObserverResumeResponse,
-    {
-        // TODO
-    },
-);
-
-request_response!(
-    RtpObserverAddProducerRequest,
-    "rtpObserver.addProducer",
-    ;,
-    RtpObserverAddProducerResponse,
-    {
-        // TODO
-    },
-);
-
-request_response!(
-    RtpObserverRemoveProducerRequest,
-    "rtpObserver.removeProducer",
-    ;,
-    RtpObserverRemoveProducerResponse,
-    {
-        // TODO
-    },
-);
+// request_response!(
+//     TransportProduceRequest,
+//     "transport.produce",
+//     ;,
+//     TransportProduceResponse,
+//     {
+//         // TODO
+//     },
+// );
+//
+// request_response!(
+//     TransportConsumeRequest,
+//     "transport.consume",
+//     ;,
+//     TransportConsumeResponse,
+//     {
+//         // TODO
+//     },
+// );
+//
+// request_response!(
+//     TransportProduceDataRequest,
+//     "transport.produceData",
+//     ;,
+//     TransportProduceDataResponse,
+//     {
+//         // TODO
+//     },
+// );
+//
+// request_response!(
+//     TransportConsumeDataRequest,
+//     "transport.consumeData",
+//     ;,
+//     TransportConsumeDataResponse,
+//     {
+//         // TODO
+//     },
+// );
+//
+// request_response!(
+//     TransportEnableTraceEventRequest,
+//     "transport.enableTraceEvent",
+//     ;,
+//     TransportEnableTraceEventResponse,
+//     {
+//         // TODO
+//     },
+// );
+//
+// request_response!(
+//     ProducerCloseRequest,
+//     "producer.close",
+//     ;,
+//     ProducerCloseResponse,
+//     {
+//         // TODO
+//     },
+// );
+//
+// request_response!(
+//     ProducerDumpRequest,
+//     "producer.dump",
+//     ;,
+//     ProducerDumpResponse,
+//     {
+//         // TODO
+//     },
+// );
+//
+// request_response!(
+//     ProducerGetStatsRequest,
+//     "producer.getStats",
+//     ;,
+//     ProducerGetStatsResponse,
+//     {
+//         // TODO
+//     },
+// );
+//
+// request_response!(
+//     ProducerPauseRequest,
+//     "producer.pause",
+//     ;,
+//     ProducerPauseResponse,
+//     {
+//         // TODO
+//     },
+// );
+//
+// request_response!(
+//     ProducerResumeRequest,
+//     "producer.resume",
+//     ;,
+//     ProducerResumeResponse,
+//     {
+//         // TODO
+//     },
+// );
+//
+// request_response!(
+//     ProducerEnableTraceEventRequest,
+//     "producer.enableTraceEvent",
+//     ;,
+//     ProducerEnableTraceEventResponse,
+//     {
+//         // TODO
+//     },
+// );
+//
+// request_response!(
+//     ConsumerCloseRequest,
+//     "consumer.close",
+//     ;,
+//     ConsumerCloseResponse,
+//     {
+//         // TODO
+//     },
+// );
+//
+// request_response!(
+//     ConsumerDumpRequest,
+//     "consumer.dump",
+//     ;,
+//     ConsumerDumpResponse,
+//     {
+//         // TODO
+//     },
+// );
+//
+// request_response!(
+//     ConsumerGetStatsRequest,
+//     "consumer.getStats",
+//     ;,
+//     ConsumerGetStatsResponse,
+//     {
+//         // TODO
+//     },
+// );
+//
+// request_response!(
+//     ConsumerPauseRequest,
+//     "consumer.pause",
+//     ;,
+//     ConsumerPauseResponse,
+//     {
+//         // TODO
+//     },
+// );
+//
+// request_response!(
+//     ConsumerResumeRequest,
+//     "consumer.resume",
+//     ;,
+//     ConsumerResumeResponse,
+//     {
+//         // TODO
+//     },
+// );
+//
+// request_response!(
+//     ConsumerSetPreferredLayersRequest,
+//     "consumer.setPreferredLayers",
+//     ;,
+//     ConsumerSetPreferredLayersResponse,
+//     {
+//         // TODO
+//     },
+// );
+//
+// request_response!(
+//     ConsumerSetPriorityRequest,
+//     "consumer.setPriority",
+//     ;,
+//     ConsumerSetPriorityResponse,
+//     {
+//         // TODO
+//     },
+// );
+//
+// request_response!(
+//     ConsumerRequestKeyFrameRequest,
+//     "consumer.requestKeyFrame",
+//     ;,
+//     ConsumerRequestKeyFrameResponse,
+//     {
+//         // TODO
+//     },
+// );
+//
+// request_response!(
+//     ConsumerEnableTraceEventRequest,
+//     "consumer.enableTraceEvent",
+//     ;,
+//     ConsumerEnableTraceEventResponse,
+//     {
+//         // TODO
+//     },
+// );
+//
+// request_response!(
+//     DataProducerCloseRequest,
+//     "dataProducer.close",
+//     ;,
+//     DataProducerCloseResponse,
+//     {
+//         // TODO
+//     },
+// );
+//
+// request_response!(
+//     DataProducerDumpRequest,
+//     "dataProducer.dump",
+//     ;,
+//     DataProducerDumpResponse,
+//     {
+//         // TODO
+//     },
+// );
+//
+// request_response!(
+//     DataProducerGetStatsRequest,
+//     "dataProducer.getStats",
+//     ;,
+//     DataProducerGetStatsResponse,
+//     {
+//         // TODO
+//     },
+// );
+//
+// request_response!(
+//     DataConsumerCloseRequest,
+//     "dataConsumer.close",
+//     ;,
+//     DataConsumerCloseResponse,
+//     {
+//         // TODO
+//     },
+// );
+//
+// request_response!(
+//     DataConsumerDumpRequest,
+//     "dataConsumer.dump",
+//     ;,
+//     DataConsumerDumpResponse,
+//     {
+//         // TODO
+//     },
+// );
+//
+// request_response!(
+//     DataConsumerGetStatsRequest,
+//     "dataConsumer.getStats",
+//     ;,
+//     DataConsumerGetStatsResponse,
+//     {
+//         // TODO
+//     },
+// );
+//
+// request_response!(
+//     DataConsumerGetBufferedAmountRequest,
+//     "dataConsumer.getBufferedAmount",
+//     ;,
+//     DataConsumerGetBufferedAmountResponse,
+//     {
+//         // TODO
+//     },
+// );
+//
+// request_response!(
+//     DataConsumerSetBufferedAmountLowThresholdRequest,
+//     "dataConsumer.setBufferedAmountLowThreshold",
+//     ;,
+//     DataConsumerSetBufferedAmountLowThresholdResponse,
+//     {
+//         // TODO
+//     },
+// );
+//
+// request_response!(
+//     RtpObserverCloseRequest,
+//     "rtpObserver.close",
+//     ;,
+//     RtpObserverCloseResponse,
+//     {
+//         // TODO
+//     },
+// );
+//
+// request_response!(
+//     RtpObserverPauseRequest,
+//     "rtpObserver.pause",
+//     ;,
+//     RtpObserverPauseResponse,
+//     {
+//         // TODO
+//     },
+// );
+//
+// request_response!(
+//     RtpObserverResumeRequest,
+//     "rtpObserver.resume",
+//     ;,
+//     RtpObserverResumeResponse,
+//     {
+//         // TODO
+//     },
+// );
+//
+// request_response!(
+//     RtpObserverAddProducerRequest,
+//     "rtpObserver.addProducer",
+//     ;,
+//     RtpObserverAddProducerResponse,
+//     {
+//         // TODO
+//     },
+// );
+//
+// request_response!(
+//     RtpObserverRemoveProducerRequest,
+//     "rtpObserver.removeProducer",
+//     ;,
+//     RtpObserverRemoveProducerResponse,
+//     {
+//         // TODO
+//     },
+// );
