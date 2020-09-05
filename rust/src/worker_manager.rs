66fc046c64734b6288a5f8fcd4baef3b2c0d92ab
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2020-09-06 00:19:45 +0300

    Method for attaching event handler for new workers on manager

--- rust/src/worker_manager.rs
+++ rust/src/worker_manager.rs
@@ -4,10 +4,16 @@ use async_oneshot::Sender;
 use futures_lite::future;
 use std::io;
 use std::path::PathBuf;
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
+
+#[derive(Default)]
+struct Handlers {
+    new_worker: Mutex<Vec<Box<dyn Fn(&Arc<Worker>)>>>,
+}
 
 pub struct WorkerManager {
     executor: Arc<Executor>,
+    handlers: Handlers,
     /// This field is only used in order to be dropped with the worker manager itself to stop the
     /// thread created with `WorkerManager::new()` call
     _stop_sender: Option<Sender<()>>,
@@ -25,16 +31,23 @@ impl WorkerManager {
                 drop(future::block_on(executor.run(stop_receiver)));
             });
         }
+
+        let handlers = Handlers::default();
+
         Self {
             executor,
+            handlers,
             _stop_sender: Some(stop_sender),
         }
     }
 
     /// Create new worker manager, uses externally created executor
     pub fn with_executor(executor: Arc<Executor>) -> Self {
+        let handlers = Handlers::default();
+
         Self {
             executor,
+            handlers,
             _stop_sender: None,
         }
     }
@@ -44,8 +57,25 @@ impl WorkerManager {
         &self,
         worker_binary: PathBuf,
         worker_settings: WorkerSettings,
-    ) -> io::Result<Worker> {
-        Worker::new(Arc::clone(&self.executor), worker_binary, worker_settings).await
+    ) -> io::Result<Arc<Worker>> {
+        let worker = Arc::new(
+            Worker::new(Arc::clone(&self.executor), worker_binary, worker_settings).await?,
+        );
+        {
+            for callback in self.handlers.new_worker.lock().unwrap().iter() {
+                callback(&worker);
+            }
+        }
+
+        Ok(worker)
+    }
+
+    pub fn connect_new_worker<F: Fn(&Arc<Worker>) + 'static>(&self, callback: F) {
+        self.handlers
+            .new_worker
+            .lock()
+            .unwrap()
+            .push(Box::new(callback));
     }
 }
 
