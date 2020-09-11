3f51ebd54616926872b4746520c65e876c622f8f
Matt Krick <matt.krick@gmail.com>
2020-09-11 02:02:06 -0700

    add MEDIASOUP_MAX_CORES to limit NUM_CORES during build (#462)
    Signed-off-by: Matt Krick <matt.krick@gmail.com>

--- worker/scripts/cpu_cores.sh
+++ worker/scripts/cpu_cores.sh
@@ -11,4 +11,8 @@ case "${OS}" in
 	*)        NUM_CORES=1;;
 esac
 
+if [ -n "${MEDIASOUP_MAX_CORES}" ]; then
+	NUM_CORES=$((MEDIASOUP_MAX_CORES>NUM_CORES ? NUM_CORES : MEDIASOUP_MAX_CORES))
+fi
+
 echo ${NUM_CORES}
