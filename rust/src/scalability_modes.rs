4dbc55b02ea67f66b6e680075a7bc438db4e7607
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2020-11-23 03:53:13 +0200

    Fix parsing scalability modes, add corresponding tests

--- rust/src/scalability_modes.rs
+++ rust/src/scalability_modes.rs
@@ -1,13 +1,8 @@
 use once_cell::sync::OnceCell;
 use regex::Regex;
+use serde::{Deserialize, Serialize};
 
-static SCALABILITY_MODE_REGEX: OnceCell<Regex> = OnceCell::new();
-
-fn get_scalability_mode_regex_regex() -> &'static Regex {
-    &SCALABILITY_MODE_REGEX
-        .get_or_init(|| Regex::new(r"^[LS]([1-9]\\d?)T([1-9]\\d?)(_KEY)?").unwrap())
-}
-
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Deserialize, Serialize)]
 pub struct ScalabilityMode {
     pub spatial_layers: u8,
     pub temporal_layers: u8,
@@ -24,9 +19,11 @@ impl Default for ScalabilityMode {
     }
 }
 
-// TODO: No tests for this now, may not work properly
 pub fn parse(scalability_mode: &str) -> ScalabilityMode {
-    get_scalability_mode_regex_regex()
+    static SCALABILITY_MODE_REGEX: OnceCell<Regex> = OnceCell::new();
+
+    SCALABILITY_MODE_REGEX
+        .get_or_init(|| Regex::new(r"^[LS]([1-9][0-9]?)T([1-9][0-9]?)(_KEY)?").unwrap())
         .captures(scalability_mode)
         .map(|captures| ScalabilityMode {
             spatial_layers: captures.get(1).unwrap().as_str().parse().unwrap(),
@@ -34,4 +31,102 @@ pub fn parse(scalability_mode: &str) -> ScalabilityMode {
             ksvc: captures.get(3).is_some(),
         })
         .unwrap_or_default()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_scalability_modes() {
+        assert_eq!(
+            parse("L1T3"),
+            ScalabilityMode {
+                spatial_layers: 1,
+                temporal_layers: 3,
+                ksvc: false
+            }
+        );
+
+        assert_eq!(
+            parse("L3T2_KEY"),
+            ScalabilityMode {
+                spatial_layers: 3,
+                temporal_layers: 2,
+                ksvc: true
+            }
+        );
+
+        assert_eq!(
+            parse("S2T3"),
+            ScalabilityMode {
+                spatial_layers: 2,
+                temporal_layers: 3,
+                ksvc: false
+            }
+        );
+
+        assert_eq!(
+            parse("foo"),
+            ScalabilityMode {
+                spatial_layers: 1,
+                temporal_layers: 1,
+                ksvc: false
+            }
+        );
+
+        assert_eq!(
+            parse("ull"),
+            ScalabilityMode {
+                spatial_layers: 1,
+                temporal_layers: 1,
+                ksvc: false
+            }
+        );
+
+        assert_eq!(
+            parse("S0T3"),
+            ScalabilityMode {
+                spatial_layers: 1,
+                temporal_layers: 1,
+                ksvc: false
+            }
+        );
+
+        assert_eq!(
+            parse("S1T0"),
+            ScalabilityMode {
+                spatial_layers: 1,
+                temporal_layers: 1,
+                ksvc: false
+            }
+        );
+
+        assert_eq!(
+            parse("L20T3"),
+            ScalabilityMode {
+                spatial_layers: 20,
+                temporal_layers: 3,
+                ksvc: false
+            }
+        );
+
+        assert_eq!(
+            parse("S200T3"),
+            ScalabilityMode {
+                spatial_layers: 1,
+                temporal_layers: 1,
+                ksvc: false
+            }
+        );
+
+        assert_eq!(
+            parse("L4T7_KEY_SHIFT"),
+            ScalabilityMode {
+                spatial_layers: 4,
+                temporal_layers: 7,
+                ksvc: true
+            }
+        );
+    }
 }
