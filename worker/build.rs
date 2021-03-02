a4842d4632cf42286312a51e7bbfa95f64ee70bb
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2021-03-03 00:59:21 +0200

    Add functional overall Rust-based worker executable build

---
+++ worker/build.rs
@@ -0,0 +1,33 @@
+use std::process::Command;
+
+fn main() {
+    // Build
+    if !Command::new("make")
+        .arg("libmediasoup-worker")
+        .spawn()
+        .expect("Failed to start")
+        .wait()
+        .expect("Wasn't running")
+        .success()
+    {
+        panic!("Failed to build libmediasoup-worker")
+    }
+
+    println!("cargo:rustc-link-lib=static=stdc++");
+    println!("cargo:rustc-link-lib=static=netstring");
+    println!("cargo:rustc-link-lib=static=uv");
+    println!("cargo:rustc-link-lib=static=openssl");
+    println!("cargo:rustc-link-lib=static=srtp");
+    println!("cargo:rustc-link-lib=static=usrsctp");
+    println!("cargo:rustc-link-lib=static=webrtc");
+    println!("cargo:rustc-link-lib=static=mediasoup-worker");
+    println!("cargo:rustc-link-lib=static=abseil");
+    println!(
+        "cargo:rustc-link-search=native={}/out/Release",
+        std::env::current_dir()
+            .unwrap()
+            .into_os_string()
+            .into_string()
+            .unwrap()
+    );
+}
