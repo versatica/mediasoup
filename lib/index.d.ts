997574af12740791a070c34e3ba4c6fbf3380d11
Iñaki Baz Castillo <ibc@aliax.net>
2021-05-31 15:15:46 +0200

    3.7.8

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -9,7 +9,7 @@ export { types };
 /**
  * Expose mediasoup version.
  */
-export declare const version = "3.7.7";
+export declare const version = "3.7.8";
 /**
  * Expose parseScalabilityMode() function.
  */
