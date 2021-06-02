fc0fdd42fe96d6f01ad04304dffa78175340ffa6
Iñaki Baz Castillo <ibc@aliax.net>
2021-06-02 02:23:26 +0200

    Let's use this PR

--- worker/src/DepLibUV.cpp
+++ worker/src/DepLibUV.cpp
@@ -5,44 +5,9 @@
 #include "Logger.hpp"
 #include <cstdlib> // std::abort()
 
-#include <iostream>
-#include <fstream>
-
-using std::cout; using std::ofstream;
-using std::endl; using std::string;
-using std::cerr;
-using std::fstream;
-
 /* Static variables. */
 
 thread_local uv_loop_t* DepLibUV::loop{ nullptr };
-
-/* Static methods for UV callbacks. */
-
-inline static void onClose(uv_handle_t* handle)
-{
-	delete handle;
-}
-
-inline static void onWalk(uv_handle_t* handle, void* arg)
-{
-	MS_DUMP(
-		"---- handle [type:%d, active:%d, closing:%d, has_ref:%d]",
-		handle->type,
-		uv_is_active(handle),
-		uv_is_closing(handle),
-		uv_has_ref(handle)
-	);
-
-	string filename("/tmp/kk.txt");
-	fstream output_fstream;
-
-	output_fstream.open(filename, std::ios_base::app);
-	output_fstream << "handle type:" << handle->type << endl;
-
-	if (!uv_is_closing(handle))
-		uv_close(handle, onClose);
-}
 
 /* Static methods. */
 
@@ -62,33 +27,8 @@ void DepLibUV::ClassDestroy()
 {
 	MS_TRACE();
 
-	// This should never happen.
-	if (DepLibUV::loop != nullptr)
-	{
-		// uv_loop_close(DepLibUV::loop);
-		// delete DepLibUV::loop;
-
-
-		int err;
-
-		uv_stop(DepLibUV::loop);
-		uv_walk(DepLibUV::loop, onWalk, nullptr);
-
-		while (true)
-		{
-			err = uv_loop_close(DepLibUV::loop);
-
-			if (err != UV_EBUSY)
-				break;
-
-			uv_run(DepLibUV::loop, UV_RUN_NOWAIT);
-		}
-
-		if (err != 0)
-			MS_ABORT("failed to close libuv loop: %s", uv_err_name(err));
-
-		delete DepLibUV::loop;
-	}
+	uv_loop_close(DepLibUV::loop);
+	delete DepLibUV::loop;
 }
 
 void DepLibUV::PrintVersion()
