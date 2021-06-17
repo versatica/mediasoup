35a8d25b86b486fc8d0bfd9ec8e4bfbd6ee34954
Maria Tverdostup <mariat@accretivetg.com>
2021-06-16 21:55:57 -0700

    Build @livelyvideo/mediasoup@3.6.34-lv5 for transcode (shm leak fix), use with @livelyvideo/pvc-packages@7.2.3

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -9,7 +9,7 @@ export { types };
 /**
  * Expose mediasoup version.
  */
-export declare const version = "3.6.34-lv4-mariat7";
+export declare const version = "3.6.34-lv5";
 /**
  * Expose parseScalabilityMode() function.
  */
