37111510cd1938c80bbfe6936057e1bee65d0eb3
Vittorio Palmisano <vittorio.palmisano@hopin.to>
2021-07-07 16:53:29 +0200

    openssl upgraded to version 1.1.1k

--- worker/deps/openssl/config/archs/linux-aarch64/no-asm/crypto/buildinf.h
+++ worker/deps/openssl/config/archs/linux-aarch64/no-asm/crypto/buildinf.h
@@ -11,7 +11,7 @@
  */
 
 #define PLATFORM "platform: linux-aarch64"
-#define DATE "built on: Fri Sep 13 15:59:08 2019 UTC"
+#define DATE "built on: Thu Mar 25 15:43:17 2021 UTC"
 
 /*
  * Generate compiler_flags as an array of individual characters. This is a
