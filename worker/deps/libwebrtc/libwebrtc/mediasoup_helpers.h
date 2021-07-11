c16c396ca61e65cf3d08fc4c8dd2e9222a0face0
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2021-07-11 06:46:06 +0300

    Remove extra semicolons

--- worker/deps/libwebrtc/libwebrtc/mediasoup_helpers.h
+++ worker/deps/libwebrtc/libwebrtc/mediasoup_helpers.h
@@ -27,20 +27,20 @@ namespace mediasoup_helpers
 			}
 
 			return receivedPackets;
-		};
+		}
 
 		// Get the reference time in microseconds, including any precision loss.
 		int64_t GetBaseTimeUs(const RTC::RTCP::FeedbackRtpTransportPacket* packet)
 		{
 			return packet->GetReferenceTimestamp() * 1000;
-		};
+		}
 
 		// Get the unwrapped delta between current base time and |prev_timestamp_us|.
 		int64_t GetBaseDeltaUs(
 		  const RTC::RTCP::FeedbackRtpTransportPacket* packet, int64_t prev_timestamp_us)
 		{
 			return GetBaseTimeUs(packet) - prev_timestamp_us;
-		};
+		}
 	} // namespace FeedbackRtpTransport
 } // namespace mediasoup_helpers
 
