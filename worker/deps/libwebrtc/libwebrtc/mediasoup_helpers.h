f5604995a00b6572bbaf08aa7365bd0ae12a86b9
penguinol <penguinol@163.com>
2022-09-05 23:01:54 +0800

    fix negative arrived_time_ms

--- worker/deps/libwebrtc/libwebrtc/mediasoup_helpers.h
+++ worker/deps/libwebrtc/libwebrtc/mediasoup_helpers.h
@@ -15,6 +15,8 @@ namespace mediasoup_helpers
 	 */
 	namespace FeedbackRtpTransport
 	{
+		constexpr int64_t kTimeWrapPeriodUs = (1ll << 24) * 64 * 1000;
+		
 		const std::vector<webrtc::rtcp::ReceivedPacket> GetReceivedPackets(
 			const RTC::RTCP::FeedbackRtpTransportPacket* packet)
 		{
@@ -35,11 +37,22 @@ namespace mediasoup_helpers
 			return packet->GetReferenceTimestamp() * 1000;
 		}
 
-		// Get the unwrapped delta between current base time and |prev_timestamp_us|.
+		// Get the wrapped delta between current base time and |prev_timestamp_us|.
 		int64_t GetBaseDeltaUs(
 		  const RTC::RTCP::FeedbackRtpTransportPacket* packet, int64_t prev_timestamp_us)
 		{
-			return GetBaseTimeUs(packet) - prev_timestamp_us;
+			int64_t delta = GetBaseTimeUs(packet) - prev_timestamp_us;
+			
+			if (std::abs(delta - kTimeWrapPeriodUs) < std::abs(delta))
+			{
+				delta -= kTimeWrapPeriodUs;
+			}
+			else if (std::abs(delta + kTimeWrapPeriodUs) < std::abs(delta))
+			{
+				delta += kTimeWrapPeriodUs;
+			}
+
+			return delta;
 		}
 	} // namespace FeedbackRtpTransport
 } // namespace mediasoup_helpers
