fa253116aaba1dcaa85285279a8a4b967e83853a
Iñaki Baz Castillo <ibc@aliax.net>
2022-09-27 13:14:55 +0200

    cosmetic

--- worker/src/RTC/RtpPacket.cpp
+++ worker/src/RTC/RtpPacket.cpp
@@ -415,14 +415,18 @@ namespace RTC
 			if (type == 1u)
 			{
 				if (extension.id == 0 || extension.id > 14 || extension.len == 0 || extension.len > 16)
+				{
 					continue;
+				}
 
 				extensionsTotalSize += (1 + extension.len);
 			}
 			else if (type == 2u)
 			{
 				if (extension.id == 0)
+				{
 					continue;
+				}
 
 				extensionsTotalSize += (2 + extension.len);
 			}
