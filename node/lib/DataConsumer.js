a837a6a9acf1f597253e853fafb3b7993ef6714d
José Luis Millán <jmillan@aliax.net>
2022-11-16 17:41:35 +0100

    update js file

--- node/lib/DataConsumer.js
+++ node/lib/DataConsumer.js
@@ -153,7 +153,7 @@ class DataConsumer extends EnhancedEventEmitter_1.EnhancedEventEmitter {
         /* Build Request. */
         const builder = this.#channel.bufferBuilder;
         const requestOffset = new FbsDataConsumer.SetBufferedAmountLowThresholdRequestT(threshold).pack(builder);
-        this.#channel.requestBinary(FbsRequest.Method.DATA_CONSUMER_SET_BUFFERED_AMOUNT_LOW_THRESHOLD, FbsRequest.Body.FBS_DataConsumer_SetBufferedAmountLowThresholdRequest, requestOffset, this.#internal.dataConsumerId).catch(() => { });
+        await this.#channel.requestBinary(FbsRequest.Method.DATA_CONSUMER_SET_BUFFERED_AMOUNT_LOW_THRESHOLD, FbsRequest.Body.FBS_DataConsumer_SetBufferedAmountLowThresholdRequest, requestOffset, this.#internal.dataConsumerId);
     }
     /**
      * Send data.
