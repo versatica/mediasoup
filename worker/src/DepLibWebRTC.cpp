e54567512a76fe6b072c62dc279cc96149fbe6ce
Eugene Voityuk <voitiuk@miro.com>
2022-12-02 14:24:52 +0200

    Update default libwebrtc fieldTrial string.

--- worker/src/DepLibWebRTC.cpp
+++ worker/src/DepLibWebRTC.cpp
@@ -3,14 +3,13 @@
 
 #include "DepLibWebRTC.hpp"
 #include "Logger.hpp"
+#include "Settings.hpp"
 #include "system_wrappers/source/field_trial.h" // webrtc::field_trial
 #include <mutex>
 
 /* Static. */
 
 static std::once_flag globalInitOnce;
-static constexpr char FieldTrials[] =
-  "WebRTC-Bwe-AlrLimitedBackoff/Enabled/WebRTC-Bwe-LossBasedBweV2/Enabled:true,CandidateFactors:1.02|1.0|0.95,DelayBasedCandidate:true,HigherBwBiasFactor:0.0002,HigherLogBwBiasFactor:0.02,ObservationDurationLowerBound:250ms,InstantUpperBoundBwBalance:75kbps,BwRampupUpperBoundFactor:1000000.0,InstantUpperBoundTemporalWeightFactor:0.9,TemporalWeightFactor:0.9,MaxIncreaseFactor:1.3,NewtonStepSize:0.75,InherentLossUpperBoundBwBalance:75kbps,LossThresholdOfHighBandwidthPreference:0.15,NotIncreaseIfInherentLossLessThanAverageLoss:true,TrendlineIntegrationEnabled:true,SendingRateSmoothingFactor:0.6/";
 
 /* Static methods. */
 
@@ -18,7 +17,16 @@ void DepLibWebRTC::ClassInit()
 {
 	MS_TRACE();
 
-	std::call_once(globalInitOnce, [] { webrtc::field_trial::InitFieldTrialsFromString(FieldTrials); });
+	MS_DEBUG_TAG(
+	  info, "libwebrtc field trials: \"%s\"", Settings::configuration.libwebrtcFieldTrials.c_str());
+
+	std::call_once(
+	  globalInitOnce,
+	  []
+	  {
+		  webrtc::field_trial::InitFieldTrialsFromString(
+		    Settings::configuration.libwebrtcFieldTrials.c_str());
+	  });
 }
 
 void DepLibWebRTC::ClassDestroy()
