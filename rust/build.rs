ef9144ac428f307f5be8794f51d1232124c60656
José Luis Millán <jmillan@aliax.net>
2023-01-24 11:45:18 +0100

    Rust: compile flatbuffers schemas during build process

---
+++ rust/build.rs
@@ -0,0 +1,28 @@
+use flatc_rust;
+use std::fs;
+use std::path::Path;
+
+/// NOTE: flatc binary path must be present in PATH environment variable.
+fn main() {
+    println!("cargo:rerun-if-changed=../fbs/");
+
+    // Retrieve a Vec<&Path> with the schema files.
+    let schemas_path = fs::read_dir("../fbs").unwrap();
+    let mut inputs_str: Vec<String> = vec![];
+    let mut inputs_path: Vec<&Path> = vec![];
+
+    for schema in schemas_path {
+        inputs_str.push(schema.unwrap().path().to_string_lossy().to_string());
+    }
+
+    for (index, _) in inputs_str.iter().enumerate() {
+        inputs_path.push(Path::new(&inputs_str[index]));
+    }
+
+    // Generate flatbuffers code.
+    flatc_rust::run(flatc_rust::Args {
+        inputs: &inputs_path,
+        out_dir: Path::new("./flatbuffers/"),
+        ..Default::default()
+    }).expect("flatc");
+}
