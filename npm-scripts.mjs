0e3bc52f21e61134857d7d7745110f9eb785ca15
Iñaki Baz Castillo <ibc@aliax.net>
2023-05-29 12:58:58 +0200

    Move npm-scripts.js to npm-scripts.mjs (ES Module)
    Avoid CommonJS gradually and use ES Module (import/export). This change is also needed ot be able to use `node-fetch` version 3.

--- npm-scripts.mjs
+++ npm-scripts.mjs
@@ -1,9 +1,9 @@
-const process = require('process');
-const os = require('os');
-const fs = require('fs');
-const { execSync, spawnSync } = require('child_process');
-const fetch = require('node-fetch');
-const tar = require('tar');
+import process from 'process';
+import os from 'os';
+import fs from 'fs';
+import { execSync, spawnSync } from 'child_process';
+import fetch from 'node-fetch';
+import tar from 'tar';
 
 const PKG = JSON.parse(fs.readFileSync('./package.json').toString());
 const IS_FREEBSD = os.platform() === 'freebsd';
@@ -98,8 +98,8 @@ async function run()
 
 		case 'typescript:watch':
 		{
-			// NOTE: Load dep here since it's a devDependency.
-			const { TscWatchClient } = require('tsc-watch/client');
+			// NOTE: Load dep on demand since it's a devDependency.
+			const { TscWatchClient } = await import('tsc-watch/client.js');
 
 			const watch = new TscWatchClient();
 
@@ -200,8 +200,8 @@ async function run()
 
 			try
 			{
-				octokit = getOctokit();
-				versionChanges = getVersionChanges();
+				octokit = await getOctokit();
+				versionChanges = await getVersionChanges();
 			}
 			catch (error)
 			{
@@ -357,7 +357,7 @@ function lintNode()
 {
 	logInfo('lintNode()');
 
-	executeCmd('eslint -c node/.eslintrc.js --max-warnings 0 node/src node/.eslintrc.js npm-scripts.js worker/scripts/gulpfile.js');
+	executeCmd('eslint -c node/.eslintrc.js --max-warnings 0 node/src node/.eslintrc.js npm-scripts.mjs worker/scripts/gulpfile.js');
 }
 
 function lintWorker()
@@ -599,7 +599,7 @@ async function uploadMacArmPrebuiltWorker()
 		return;
 	}
 
-	const octokit = getOctokit();
+	const octokit = await getOctokit();
 
 	logInfo('uploadMacArmPrebuiltWorker() | getting release info');
 
@@ -623,15 +623,15 @@ async function uploadMacArmPrebuiltWorker()
 		});
 }
 
-function getOctokit()
+async function getOctokit()
 {
 	if (!process.env.GITHUB_TOKEN)
 	{
 		throw new Error('missing GITHUB_TOKEN environment variable');
 	}
 
-	// NOTE: Load dep here since it's a devDependency.
-	const { Octokit } = require('@octokit/rest');
+	// NOTE: Load dep on demand since it's a devDependency.
+	const { Octokit } = await import('@octokit/rest');
 
 	const octokit = new Octokit(
 		{
@@ -641,12 +641,12 @@ function getOctokit()
 	return octokit;
 }
 
-function getVersionChanges()
+async function getVersionChanges()
 {
 	logInfo('getVersionChanges()');
 
-	// NOTE: Load dep here since it's a devDependency.
-	const marked = require('marked');
+	// NOTE: Load dep on demand since it's a devDependency.
+	const marked = await import('marked');
 
 	const changelog = fs.readFileSync('./CHANGELOG.md').toString();
 	const entries = marked.lexer(changelog);
@@ -692,19 +692,19 @@ function executeCmd(command, exitOnError = true)
 function logInfo(message)
 {
 	// eslint-disable-next-line no-console
-	console.log(`npm-scripts.js \x1b[36m[INFO] [${task}]\x1b\[0m`, message);
+	console.log(`npm-scripts \x1b[36m[INFO] [${task}]\x1b\[0m`, message);
 }
 
 function logWarn(message)
 {
 	// eslint-disable-next-line no-console
-	console.warn(`npm-scripts.js \x1b[33m[WARN] [${task}]\x1b\[0m`, message);
+	console.warn(`npm-scripts \x1b[33m[WARN] [${task}]\x1b\[0m`, message);
 }
 
 function logError(message)
 {
 	// eslint-disable-next-line no-console
-	console.error(`npm-scripts.js \x1b[31m[ERROR] [${task}]\x1b\[0m`, message);
+	console.error(`npm-scripts \x1b[31m[ERROR] [${task}]\x1b\[0m`, message);
 }
 
 function exitWithError()
