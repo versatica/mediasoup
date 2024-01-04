e51b47bdbfd3bd4d23d3288ab4832c10f5a662b1
Iñaki Baz Castillo <ibc@aliax.net>
2024-01-04 12:34:48 +0100

    Make Node tests independent (part 2) (#1296)
    - Just done in `test-Router.ts` as POC.
    - Avoid declaring global variables in test files. Put them into `ctx: TestContext` instead.
    - Things in `ctx` should be const/frozen or reset in each `test()` execution.
    - So use a new `utils.deepFreeze()` function which freezes the object and would throw upon any  modification attempt.

--- node/src/utils.ts
+++ node/src/utils.ts
@@ -197,3 +197,26 @@ export function parseStringStringArrayVector(
 
 	return array;
 }
+
+/**
+ * Make an object or array recursively immutable.
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/freeze.
+ */
+export function deepFreeze<T>(object: T): T
+{
+	// Retrieve the property names defined on object.
+	const propNames = Reflect.ownKeys(object as any);
+
+	// Freeze properties before freezing self.
+	for (const name of propNames)
+	{
+		const value = (object as any)[name];
+
+		if ((value && typeof value === 'object') || typeof value === 'function')
+		{
+			deepFreeze(value);
+		}
+	}
+
+	return Object.freeze(object);
+}
