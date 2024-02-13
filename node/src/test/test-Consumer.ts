053177d09504f4f9ee63fb2826aee090c3ef936e
Jesús Leganés-Combarro <piranna@gmail.com>
2024-02-13 16:39:48 +0100

    Remove `no-async-promise-executor` linter disables (#1312)

--- node/src/test/test-Consumer.ts
+++ node/src/test/test-Consumer.ts
@@ -1,3 +1,4 @@
+import { once } from 'node:events';
 import * as flatbuffers from 'flatbuffers';
 import * as mediasoup from '../';
 import { UnsupportedError } from '../errors';
@@ -993,26 +994,24 @@ test('Consumer emits "producerpause" and "producerresume"', async () => {
 		rtpCapabilities: ctx.consumerDeviceCapabilities,
 	});
 
-	// eslint-disable-next-line no-async-promise-executor
-	await new Promise<void>(async resolve => {
-		audioConsumer.on('producerpause', resolve);
+	await Promise.all([
+		once(audioConsumer, 'producerpause'),
 
 		// Let's await for pause() to resolve to avoid aborted channel requests
 		// due to worker closure.
-		await ctx.audioProducer!.pause();
-	});
+		ctx.audioProducer!.pause(),
+	]);
 
 	expect(audioConsumer.paused).toBe(false);
 	expect(audioConsumer.producerPaused).toBe(true);
 
-	// eslint-disable-next-line no-async-promise-executor
-	await new Promise<void>(async resolve => {
-		audioConsumer.on('producerresume', resolve);
+	await Promise.all([
+		once(audioConsumer, 'producerresume'),
 
 		// Let's await for resume() to resolve to avoid aborted channel requests
 		// due to worker closure.
-		await ctx.audioProducer!.resume();
-	});
+		ctx.audioProducer!.resume(),
+	]);
 
 	expect(audioConsumer.paused).toBe(false);
 	expect(audioConsumer.producerPaused).toBe(false);
