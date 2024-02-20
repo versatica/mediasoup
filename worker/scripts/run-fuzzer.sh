eb8b9d98676a09158551400e2097a4b0f6fbe17b
Iñaki Baz Castillo <ibc@aliax.net>
2024-02-20 12:47:35 +0100

    improve the script so it really monitors the fuzzer process once per second

--- worker/scripts/run-fuzzer.sh
+++ worker/scripts/run-fuzzer.sh
@@ -5,30 +5,34 @@ DURATION_SEC=$1
 
 current_dir_name=${WORKER_PWD##*/}
 if [ "${current_dir_name}" != "worker" ] ; then
-	echo ">>> [ERROR] $(basename $0) must be called from mediasoup/worker directory" >&2
+	echo "run-fuzzer.sh [ERROR] $(basename $0) must be called from mediasoup/worker directory" >&2
 	exit 1
 fi
 
-if [ "$#" -eq 0 ]; then
-	echo ">>> [ERROR] duration (in seconds) must be fiven as argument" >&2
+if [ "$#" -eq 0 ] ; then
+	echo "run-fuzzer.sh [ERROR] duration (in seconds) must be fiven as argument" >&2
 	exit 1
 fi
 
-# Run mediasoup-worker-fuzzer in background, get its pid, wait for given
-# duration and then kill mediasoup-worker-fuzzer. If still running, kill
-# will return status code 0, otherwise 1 (which means that the
-# mediasoup-worker-fuzzer binary already exited due to some fuzzer or other
-# error).
 invoke fuzzer-run-all &
-MEDIASOUP_WORKER_FUZZER_PID=$!
-sleep ${DURATION_SEC}
-kill ${MEDIASOUP_WORKER_FUZZER_PID}
-status=$?;
 
-if [ "${status}" -eq 0 ]; then
-	echo ">>> [INFO] mediasoup-worker-fuzzer was still running so there is no fuzzer issue so far"
-	exit 0
-else
-	echo ">>> [ERROR] mediasoup-worker-fuzzer was not running so it existed already due to some fuzzer issue" >&2
-	exit ${status}
-fi
+MEDIASOUP_WORKER_FUZZER_PID=$!
+
+i=${DURATION_SEC}
+
+until [ ${i} -eq 0 ]
+do
+	echo "run-fuzzer.sh [INFO] ${i} seconds left"
+	if ! kill -0 ${MEDIASOUP_WORKER_FUZZER_PID} &> /dev/null ; then
+		echo "run-fuzzer.sh [ERROR] mediasoup-worker-fuzzer died" >&2
+		exit 1
+	else
+		((i=i-1))
+		sleep 1
+	fi
+done
+
+echo "run-fuzzer.sh [INFO] mediasoup-worker-fuzzer is still running after given ${DURATION_SEC} seconds so no fuzzer issues so far"
+
+kill -SIGTERM ${MEDIASOUP_WORKER_FUZZER_PID} &> /dev/null
+exit 0
