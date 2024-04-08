f9310624c4bf2b448855cdeb7ca00e3f2274499a
Iñaki Baz Castillo <ibc@aliax.net>
2024-04-08 20:26:40 +0200

    Add a test to check issue #1366 and use Debug mode

--- worker/test/src/RTC/TestNackGenerator.cpp
+++ worker/test/src/RTC/TestNackGenerator.cpp
@@ -139,6 +139,38 @@ void validate(std::vector<TestNackGeneratorInput>& inputs)
 	}
 };
 
+SCENARIO("ISSUE 1366: https://github.com/versatica/mediasoup/issues/1366")
+{
+	SECTION("absl::btree_set")
+	{
+		REQUIRE(RTC::SeqManager<uint16_t>::IsSeqLowerThan(10000, 40000) == true);
+		REQUIRE(RTC::SeqManager<uint16_t>::IsSeqLowerThan(40000, 60000) == true);
+		REQUIRE(RTC::SeqManager<uint16_t>::IsSeqLowerThan(10000, 60000) == false);
+	}
+
+	SECTION("std::set")
+	{
+		std::set<uint16_t, RTC::SeqManager<uint16_t>::SeqLowerThan> recoveredList;
+
+		recoveredList.insert(10000);
+		recoveredList.insert(40000);
+		recoveredList.insert(60000);
+
+		REQUIRE(recoveredList.size() == 3);
+	}
+
+	// SECTION("absl::btree_set")
+	// {
+	// 	absl::btree_set<uint16_t, RTC::SeqManager<uint16_t>::SeqLowerThan> recoveredList;
+
+	// 	recoveredList.insert(10000);
+	// 	recoveredList.insert(40000);
+	// 	recoveredList.insert(60000);
+
+	// 	REQUIRE(recoveredList.size() == 3);
+	// }
+}
+
 SCENARIO("NACK generator", "[rtp][rtcp]")
 {
 	SECTION("no NACKs required")
