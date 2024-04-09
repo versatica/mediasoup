76e025a894d85a9e6c44e96475ba1c83301f8f0b
Iñaki Baz Castillo <ibc@aliax.net>
2024-04-09 10:01:22 +0200

    Only std::set

--- worker/test/src/RTC/TestNackGenerator.cpp
+++ worker/test/src/RTC/TestNackGenerator.cpp
@@ -159,16 +159,16 @@ SCENARIO("ISSUE 1366: https://github.com/versatica/mediasoup/issues/1366")
 		REQUIRE(recoveredList.size() == 3);
 	}
 
-	SECTION("absl::btree_set")
-	{
-		absl::btree_set<uint16_t, RTC::SeqManager<uint16_t>::SeqLowerThan> recoveredList;
+	// SECTION("absl::btree_set")
+	// {
+	// 	absl::btree_set<uint16_t, RTC::SeqManager<uint16_t>::SeqLowerThan> recoveredList;
 
-		recoveredList.insert(10000);
-		recoveredList.insert(40000);
-		recoveredList.insert(60000);
+	// 	recoveredList.insert(10000);
+	// 	recoveredList.insert(40000);
+	// 	recoveredList.insert(60000);
 
-		REQUIRE(recoveredList.size() == 3);
-	}
+	// 	REQUIRE(recoveredList.size() == 3);
+	// }
 }
 
 SCENARIO("NACK generator", "[rtp][rtcp]")
