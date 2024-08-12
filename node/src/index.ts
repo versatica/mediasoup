9929459e124f8d95a3ced45ff18f4a318912f95d
Iñaki Baz Castillo <ibc@aliax.net>
2024-08-12 17:42:06 +0200

    Fix WorkerSettings.disableLiburing option
    ### Details
    
    - I forgot to pass it in `createWorker()` method...

--- node/src/index.ts
+++ node/src/index.ts
@@ -56,6 +56,7 @@ export async function createWorker<
 	dtlsCertificateFile,
 	dtlsPrivateKeyFile,
 	libwebrtcFieldTrials,
+	disableLiburing,
 	appData,
 }: WorkerSettings<WorkerAppData> = {}): Promise<Worker<WorkerAppData>> {
 	logger.debug('createWorker()');
@@ -72,6 +73,7 @@ export async function createWorker<
 		dtlsCertificateFile,
 		dtlsPrivateKeyFile,
 		libwebrtcFieldTrials,
+		disableLiburing,
 		appData,
 	});
 
