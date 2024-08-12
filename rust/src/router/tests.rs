630f3b6ffa4b005026fd86100a4e7888101fd58f
Iñaki Baz Castillo <ibc@aliax.net>
2024-08-12 11:18:53 +0200

    Revert "stop please"
    This reverts commit 53b68bcea9cc721bd6c875e0dc40a01d6fa23082.

--- rust/src/router/tests.rs
+++ rust/src/router/tests.rs
@@ -16,12 +16,7 @@ async fn init() -> Worker {
     let worker_manager = WorkerManager::new();
 
     worker_manager
-        .create_worker({
-            let mut settings = WorkerSettings::default();
-            settings.enable_liburing = false;
-
-            settings
-        })
+        .create_worker(WorkerSettings::default())
         .await
         .expect("Failed to create worker")
 }
@@ -33,6 +28,12 @@ fn worker_close_event() {
 
         let router = worker
             .create_router(RouterOptions::default())
+            .create_worker({
+                let mut settings = WorkerSettings::default();
+                settings.enable_liburing = false;
+
+                settings
+            })
             .await
             .expect("Failed to create router");
 
