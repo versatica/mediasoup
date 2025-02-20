0bde5d726fe03bbe73c51d00b2d204e763f2c398
Iñaki Baz Castillo <ibc@aliax.net>
2025-02-20 16:39:49 +0100

    fix

--- worker/src/RTC/Transport.cpp
+++ worker/src/RTC/Transport.cpp
@@ -760,7 +760,7 @@ namespace RTC
 					if (createTccServer)
 					{
 						this->tccServer = std::make_shared<RTC::TransportCongestionControlServer>(
-						  this, bweType, RTC::Consts::MidRtpExtensionMaxLength);
+						  this, bweType, RTC::Consts::RtcpPacketMaxSize);
 
 						if (this->maxIncomingBitrate != 0u)
 						{
