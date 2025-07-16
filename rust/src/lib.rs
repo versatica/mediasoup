46cc3e14af579095dadb92eecc8bdb0bf65eb8ff
José Luis Millán <jmillan@aliax.net>
2025-07-16 10:31:43 +0200

    re-export types from mediasoup crate

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -48,6 +48,7 @@
 //! Please check integration and unit tests for usage examples, they cover all major functionality
 //! and are a good place to start until we have demo apps built in Rust).
 
+pub use mediasoup_types as types;
 pub mod data_structures;
 pub(crate) mod fbs;
 mod macros;
