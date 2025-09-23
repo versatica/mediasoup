1b093360c9810ef472243d8a619c8c5160e46a4c
Andrey Skryabin <nifigase@gmail.com>
2025-09-23 13:05:12 +0300

    VP8 keyframe detect fix for non-extended payloads

--- worker/test/src/RTC/Codecs/TestVP8.cpp
+++ worker/test/src/RTC/Codecs/TestVP8.cpp
@@ -243,7 +243,7 @@ SCENARIO("parse VP8 payload descriptor", "[codecs][vp8]")
 		REQUIRE_FALSE(payloadDescriptor);
 	}
 
-	SECTION("parse payload descriptor. X flag is not set")
+	SECTION("parse payload descriptor. X flag is not set, no keyframe")
 	{
 		/** VP8 Payload Descriptor
 		 *
@@ -252,24 +252,95 @@ SCENARIO("parse VP8 payload descriptor", "[codecs][vp8]")
 		 * 0 = N bit: Reference frame
 		 * 1 = S bit: Start of VP8 partition
 		 * Part Id: 0
-		 * 1 = I bit: Picture ID byte present
-		 * 0 = L bit: TL0PICIDX byte not present
-		 * 0 = T bit: TID (temporal layer index) byte not present
-		 * 0 = K bit: TID/KEYIDX byte not present
-		 * 0000 = Reserved A: 0
-		 * 0001 0001 = Picture Id: 17
+		 * 000000 = Size0 | H | VER
+		 * 1 = P bit: Inverse Keyframe
 		 */
 
 		// clang-format off
 		uint8_t buffer[] =
 		{
-			0x50, 0x80, 0x11
+			0x50, 0x01
 		};
 		// clang-format on
 
 		auto* payloadDescriptor = Codecs::VP8::Parse(buffer, sizeof(buffer));
 
-		REQUIRE_FALSE(payloadDescriptor);
+		REQUIRE(payloadDescriptor);
+
+		REQUIRE(payloadDescriptor->extended == 0);
+		REQUIRE(payloadDescriptor->nonReference == 0);
+		REQUIRE(payloadDescriptor->start == 1);
+		REQUIRE(payloadDescriptor->partitionIndex == 0);
+
+		// optional field flags.
+		REQUIRE(payloadDescriptor->i == 0);
+		REQUIRE(payloadDescriptor->l == 0);
+		REQUIRE(payloadDescriptor->t == 0);
+		REQUIRE(payloadDescriptor->k == 0);
+
+		// optional fields.
+		REQUIRE(payloadDescriptor->pictureId == 0);
+		REQUIRE(payloadDescriptor->tl0PictureIndex == 0);
+		REQUIRE(payloadDescriptor->tlIndex == 0);
+		REQUIRE(payloadDescriptor->y == 0);
+		REQUIRE(payloadDescriptor->keyIndex == 0);
+
+		REQUIRE(payloadDescriptor->isKeyFrame == false);
+		REQUIRE(payloadDescriptor->hasPictureId == false);
+		REQUIRE(payloadDescriptor->hasOneBytePictureId == false);
+		REQUIRE(payloadDescriptor->hasTwoBytesPictureId == false);
+		REQUIRE(payloadDescriptor->hasTl0PictureIndex == false);
+		REQUIRE(payloadDescriptor->hasTlIndex == false);
+	}
+
+	SECTION("parse payload descriptor. X flag is not set, keyframe")
+	{
+		/** VP8 Payload Descriptor
+		 *
+		 * 0 = X bit: Extended control bits present (I L T K)
+		 * 1 = R bit: Reserved for future use (Error should be zero)
+		 * 0 = N bit: Reference frame
+		 * 1 = S bit: Start of VP8 partition
+		 * Part Id: 0
+		 * 000000 = Size0 | H | VER
+		 * 0 = P bit: Inverse Keyframe
+		 */
+
+		// clang-format off
+		uint8_t buffer[] =
+		{
+			0x50, 0x00
+		};
+		// clang-format on
+
+		auto* payloadDescriptor = Codecs::VP8::Parse(buffer, sizeof(buffer));
+
+		REQUIRE(payloadDescriptor);
+
+		REQUIRE(payloadDescriptor->extended == 0);
+		REQUIRE(payloadDescriptor->nonReference == 0);
+		REQUIRE(payloadDescriptor->start == 1);
+		REQUIRE(payloadDescriptor->partitionIndex == 0);
+
+		// optional field flags.
+		REQUIRE(payloadDescriptor->i == 0);
+		REQUIRE(payloadDescriptor->l == 0);
+		REQUIRE(payloadDescriptor->t == 0);
+		REQUIRE(payloadDescriptor->k == 0);
+
+		// optional fields.
+		REQUIRE(payloadDescriptor->pictureId == 0);
+		REQUIRE(payloadDescriptor->tl0PictureIndex == 0);
+		REQUIRE(payloadDescriptor->tlIndex == 0);
+		REQUIRE(payloadDescriptor->y == 0);
+		REQUIRE(payloadDescriptor->keyIndex == 0);
+
+		REQUIRE(payloadDescriptor->isKeyFrame == true);
+		REQUIRE(payloadDescriptor->hasPictureId == false);
+		REQUIRE(payloadDescriptor->hasOneBytePictureId == false);
+		REQUIRE(payloadDescriptor->hasTwoBytesPictureId == false);
+		REQUIRE(payloadDescriptor->hasTl0PictureIndex == false);
+		REQUIRE(payloadDescriptor->hasTlIndex == false);
 	}
 }
 
