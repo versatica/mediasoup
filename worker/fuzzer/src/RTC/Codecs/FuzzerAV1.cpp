bf5f3354168574086cf7b6a850c45e4d1ab1719b
José Luis Millán <jmillan@aliax.net>
2025-10-02 15:10:32 +0200

    lint

--- worker/fuzzer/src/RTC/Codecs/FuzzerAV1.cpp
+++ worker/fuzzer/src/RTC/Codecs/FuzzerAV1.cpp
@@ -14,10 +14,8 @@ void Fuzzer::RTC::Codecs::AV1::Fuzz(const uint8_t* data, size_t len)
 	Listener listener;
 	std::unique_ptr<::RTC::Codecs::DependencyDescriptor::TemplateDependencyStructure> templateDependencyStructure;
 
-	auto dependencyDescriptor =
-
-	  std::unique_ptr<::RTC::Codecs::DependencyDescriptor>(::RTC::Codecs::DependencyDescriptor::Parse(
-	    data, len, &listener, templateDependencyStructure));
+	auto dependencyDescriptor = std::unique_ptr<::RTC::Codecs::DependencyDescriptor>(
+	  ::RTC::Codecs::DependencyDescriptor::Parse(data, len, &listener, templateDependencyStructure));
 
 	auto* descriptor = ::RTC::Codecs::AV1::Parse(dependencyDescriptor);
 
