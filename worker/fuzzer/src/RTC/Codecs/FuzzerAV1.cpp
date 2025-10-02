b15fba9a02d6b2334d3eed43410ab78f749360a3
José Luis Millán <jmillan@aliax.net>
2025-10-02 15:08:55 +0200

    fix fuzzer

--- worker/fuzzer/src/RTC/Codecs/FuzzerAV1.cpp
+++ worker/fuzzer/src/RTC/Codecs/FuzzerAV1.cpp
@@ -1,16 +1,25 @@
 #include "RTC/Codecs/FuzzerAV1.hpp"
 #include "RTC/Codecs/AV1.hpp"
 
+class Listener : public ::RTC::Codecs::DependencyDescriptor::Listener
+{
+public:
+	void OnDependencyDescriptorUpdated(const uint8_t* data, size_t len) override
+	{
+	}
+};
+
 void Fuzzer::RTC::Codecs::AV1::Fuzz(const uint8_t* data, size_t len)
 {
+	Listener listener;
 	std::unique_ptr<::RTC::Codecs::DependencyDescriptor::TemplateDependencyStructure> templateDependencyStructure;
 
-	auto* dependencyDescriptor =
-	  ::RTC::Codecs::DependencyDescriptor::Parse(data, len, templateDependencyStructure);
+	auto dependencyDescriptor =
+
+	  std::unique_ptr<::RTC::Codecs::DependencyDescriptor>(::RTC::Codecs::DependencyDescriptor::Parse(
+	    data, len, &listener, templateDependencyStructure));
 
 	auto* descriptor = ::RTC::Codecs::AV1::Parse(dependencyDescriptor);
-
-	delete dependencyDescriptor;
 
 	if (!descriptor)
 	{
