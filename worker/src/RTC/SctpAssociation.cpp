d5398fceb10d71657d6f8048c1480b78f3980d01
José Luis Millán <jmillan@aliax.net>
2025-11-07 13:34:10 +0100

    leftover

--- worker/src/RTC/SctpAssociation.cpp
+++ worker/src/RTC/SctpAssociation.cpp
@@ -586,6 +586,8 @@ namespace RTC
 		{
 			MS_WARN_TAG(sctp, "usrsctp_setsockopt(SCTP_RESET_STREAMS) failed: %s", std::strerror(errno));
 		}
+
+		std::free(srs);
 	}
 
 	void SctpAssociation::AddOutgoingStreams(bool force)
