5638f37512da55794fa9c6d6aa44a0dae1bbba42
José Luis Millán <jmillan@aliax.net>
2025-11-07 13:05:11 +0100

    cast varialbes when it corresponds

--- worker/src/RTC/RtpStreamRecv.cpp
+++ worker/src/RTC/RtpStreamRecv.cpp
@@ -36,14 +36,15 @@ namespace RTC
 	{
 		MS_TRACE();
 
-		size_t spatialLayer  = packet->GetSpatialLayer();
-		size_t temporalLayer = packet->GetTemporalLayer();
+		auto spatialLayer  = packet->GetSpatialLayer();
+		auto temporalLayer = packet->GetTemporalLayer();
 
 		// Sanity check. Do not allow spatial layers higher than defined.
-		spatialLayer = std::min(spatialLayer, this->spatialLayerCounters.size() - 1);
+		spatialLayer = std::min(static_cast<size_t>(spatialLayer), this->spatialLayerCounters.size() - 1);
 
 		// Sanity check. Do not allow temporal layers higher than defined.
-		temporalLayer = std::min(temporalLayer, this->spatialLayerCounters[0].size() - 1);
+		temporalLayer =
+		  std::min(static_cast<size_t>(temporalLayer), this->spatialLayerCounters[0].size() - 1);
 
 		auto& counter = this->spatialLayerCounters[spatialLayer][temporalLayer];
 
