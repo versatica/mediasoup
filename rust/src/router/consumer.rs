1009fb41c155734dd11617a3df754e27da09ae80
Iñaki Baz Castillo <ibc@aliax.net>
2025-11-13 14:23:06 +0100

    rust

--- rust/src/router/consumer.rs
+++ rust/src/router/consumer.rs
@@ -398,6 +398,7 @@ pub struct ConsumerStat {
     pub mime_type: MimeType,
     pub packets_lost: i32,
     pub fraction_lost: u8,
+    pub jitter: u32,
     pub packets_discarded: u64,
     pub packets_retransmitted: u64,
     pub packets_repaired: u64,
@@ -405,11 +406,12 @@ pub struct ConsumerStat {
     pub nack_packet_count: u64,
     pub pli_count: u64,
     pub fir_count: u64,
-    pub score: u8,
     pub packet_count: u64,
     pub byte_count: u64,
     pub bitrate: u32,
     pub round_trip_time: Option<f32>,
+    pub rtx_packets_discarded: Option<u64>,
+    pub score: u8,
 }
 
 impl FromFbs for ConsumerStat {
@@ -432,6 +434,7 @@ impl FromFbs for ConsumerStat {
             mime_type: base.mime_type.to_string().parse().unwrap(),
             packets_lost: base.packets_lost,
             fraction_lost: base.fraction_lost,
+            jitter: base.jitter,
             packets_discarded: base.packets_discarded,
             packets_retransmitted: base.packets_retransmitted,
             packets_repaired: base.packets_repaired,
@@ -439,11 +442,12 @@ impl FromFbs for ConsumerStat {
             nack_packet_count: base.nack_packet_count,
             pli_count: base.pli_count,
             fir_count: base.fir_count,
-            score: base.score,
             packet_count: stats.packet_count,
             byte_count: stats.byte_count,
             bitrate: stats.bitrate,
             round_trip_time: Some(base.round_trip_time),
+            rtx_packets_discarded: Some(base.rtx_packets_discarded),
+            score: base.score,
         }
     }
 }
