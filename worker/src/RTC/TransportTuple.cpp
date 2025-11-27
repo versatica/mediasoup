f23207d20bb83e95fb32eff28735a5a3888ee155
José Luis Millán <jmillan@aliax.net>
2025-11-27 09:39:29 +0100

    use the old good array
    std::array does not provide the number of added elements, just the total size.
    
    We need it in order to call GenerateFnv1aHash() with the proper size. Previous to this
    commit we were passing the whole array capacity rather than the real size, meaning
    extra length for IPv4 tuples (it does not hurt, but it's not correct either).
    
    Since we need an incremental index anyway there is no reason to keep using std::array.

--- worker/src/RTC/TransportTuple.cpp
+++ worker/src/RTC/TransportTuple.cpp
@@ -3,7 +3,6 @@
 
 #include "RTC/TransportTuple.hpp"
 #include "Logger.hpp"
-#include <array>
 
 namespace RTC
 {
@@ -155,10 +154,8 @@ namespace RTC
 
 		// Maximum buffer length for two IPv6 addresses and ports plus protocol.
 		static constexpr size_t BufferSize = ((16 + 2) * 2) + 1;
-		std::array<uint8_t, BufferSize> buffer{};
-		// NOTE: Windows cannot deduce the type if using 'auto'.
-		// NOLINTNEXTLINE (hicpp-use-auto)
-		std::array<uint8_t, BufferSize>::iterator it = buffer.begin();
+		uint8_t buffer[BufferSize]         = {};
+		size_t idx                         = 0;
 
 		auto appendSockAddr = [&](const sockaddr* addr)
 		{
@@ -168,11 +165,10 @@ namespace RTC
 				const auto* ip      = reinterpret_cast<const uint8_t*>(&in->sin_addr.s_addr);
 				const uint16_t port = ntohs(in->sin_port);
 
-				it  = std::copy(ip, ip + 4, it);
-				*it = (port >> 8) & 0xFF;
-				it++;
-				*it = port & 0xFF;
-				it++;
+				std::memcpy(buffer + idx, ip, 4);
+				idx += 4;
+				buffer[idx++] = (port >> 8) & 0xFF;
+				buffer[idx++] = port & 0xFF;
 			}
 			else if (addr->sa_family == AF_INET6)
 			{
@@ -180,19 +176,18 @@ namespace RTC
 				const auto* ip      = reinterpret_cast<const uint8_t*>(&in6->sin6_addr);
 				const uint16_t port = ntohs(in6->sin6_port);
 
-				it  = std::copy(ip, ip + 16, it);
-				*it = (port >> 8) & 0xFF;
-				it++;
-				*it = port & 0xFF;
-				it++;
+				std::memcpy(buffer + idx, ip, 16);
+				idx += 16;
+				buffer[idx++] = (port >> 8) & 0xFF;
+				buffer[idx++] = port & 0xFF;
 			}
 		};
 
 		appendSockAddr(localSockAddr);
 		appendSockAddr(remoteSockAddr);
 
-		*it = static_cast<uint8_t>(this->protocol);
+		buffer[idx] = static_cast<uint8_t>(this->protocol);
 
-		this->hash = TransportTuple::GenerateFnv1aHash(buffer.data(), buffer.size());
+		this->hash = TransportTuple::GenerateFnv1aHash(buffer, idx + 1);
 	}
 } // namespace RTC
