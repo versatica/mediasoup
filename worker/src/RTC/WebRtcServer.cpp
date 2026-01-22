dfd496d92cf000667666bc6060b027a4e61f1573
Iñaki Baz Castillo <ibc@aliax.net>
2026-01-22 11:59:54 +0100

    tempral for debugging

--- worker/src/RTC/WebRtcServer.cpp
+++ worker/src/RTC/WebRtcServer.cpp
@@ -6,6 +6,8 @@
 #include "MediaSoupErrors.hpp"
 #include "Settings.hpp"
 #include "Utils.hpp"
+// TODO: REMOVE
+#include "RTC/ICE/StunPacket.hpp"
 #include <cmath> // std::pow()
 
 namespace RTC
@@ -447,6 +449,17 @@ namespace RTC
 
 			return;
 		}
+
+		// TODO: REMOVE
+		RTC::ICE::StunPacket* newPacket = RTC::ICE::StunPacket::Parse(data, len);
+		MS_ASSERT(newPacket, "new STUN parser failed!!!");
+		MS_DUMP_CLEAN(0, "");
+		MS_DUMP_CLEAN(0, "------- OLD STUN PACKET:");
+		packet->Dump();
+		MS_DUMP_CLEAN(0, "");
+		MS_DUMP_CLEAN(0, "------- NEW STUN PACKET:");
+		newPacket->Dump();
+		delete newPacket;
 
 		// First try doing lookup in the tuples table.
 		auto it1 = this->mapTupleWebRtcTransport.find(tuple->hash);
