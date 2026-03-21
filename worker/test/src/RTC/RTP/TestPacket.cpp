1c6f641586c5bc0c48aa823abad6ec6fa5aee231
Iñaki Baz Castillo <ibc@aliax.net>
2026-03-21 15:03:31 +0100

    Update worker/test/src/RTC/RTP/TestPacket.cpp
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- worker/test/src/RTC/RTP/TestPacket.cpp
+++ worker/test/src/RTC/RTP/TestPacket.cpp
@@ -2162,7 +2162,7 @@ SCENARIO("RTP Packet", "[serializable][rtp][packet]")
 		bool packetBufferReleased{ false };
 
 		RTC::Serializable::BufferReleasedListener packetBufferReleasedListener =
-		  [&packetBufferReleased](const RTC::Serializable* serializable, uint8_t* serializableBuffer)
+r =const 
 		{
 			if (serializable->GetBuffer() == rtpCommon::FactoryBuffer && serializable->GetBuffer() == serializableBuffer)
 			{
