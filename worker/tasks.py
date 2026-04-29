e7fb4f3b673456ff74e4ae510214bc1ce2f6b095
Iñaki Baz Castillo <ibc@aliax.net>
2026-04-29 19:39:26 +0200

    let's try

--- worker/tasks.py
+++ worker/tasks.py
@@ -496,7 +496,7 @@ def test_asan_undefined(ctx):
         );
 
 
-@task(pre=[call(setup, meson_args=MESON_ARGS + ' -Db_sanitize=address -Db_lundef=false'), flatc])
+@task(pre=[call(setup, meson_args=MESON_ARGS + ' -Dms_build_tests=true -Db_sanitize=address -Db_lundef=false'), flatc])
 def fuzzer(ctx):
     """
     Build the mediasoup-worker-fuzzer binary (which uses libFuzzer)
