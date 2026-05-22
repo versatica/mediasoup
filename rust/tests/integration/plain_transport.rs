25a329140de8039db3125374ce04bf67e69c30ce
Iñaki Baz Castillo <ibc@aliax.net>
2026-05-22 12:12:06 +0200

    fix

--- rust/tests/integration/plain_transport.rs
+++ rust/tests/integration/plain_transport.rs
@@ -204,11 +204,7 @@ fn create_succeeds() {
                     },
                     // TODO: SCTP: Must implement `isDataChannel` in new SCTP stack, from now
                     // it's always set to `true`.
-                    is_data_channel: if *USE_BUILT_IN_SCTP_STACK {
-                        true
-                    } else {
-                        false
-                    },
+                    is_data_channel: *USE_BUILT_IN_SCTP_STACK,
                     total_buffered_amount: 0,
                 }),
             );
